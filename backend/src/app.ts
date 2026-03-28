87179d7e04c27c632c244821de514f01f3ee6d35
akovtune <akovtune@student.codam.nl>
2026-03-28 18:22:18 +0100

    feat: added hello world example

---
+++ backend/src/app.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response, NextFunction } from "express";
+import pino from "pino-http";
+import prettyFormat from "pino-pretty";
+import cors, { CorsOptions } from "cors";
+import { HttpError } from "http-errors";
+import "dotenv/config";
+
+const { FRONTEND_URL = "*" } = process.env;
+
+const corsOptions: CorsOptions = {
+  origin: FRONTEND_URL,
+  optionsSuccessStatus: 200
+};
+
+const app = express();
+
+app.use(pino(prettyFormat()));
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  return res.json({ message: "Hello world!" });
+});
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((error: unknown, _: Request, res: Response, __: NextFunction) => {
+  if (error instanceof HttpError)
+    return res.status(error.status).json({ message: error.message });
+
+  return res.status(500).json({ message: "Server error" });
+});
+
+export default app;
