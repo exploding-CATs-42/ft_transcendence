ed0e913d2ff5c35eeb6af45aaae43259b7832f1a
akovtune <akovtune@student.codam.nl>
2026-04-01 18:45:15 +0200

    chore: configured eslint

---
+++ frontend/eslint.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default defineConfig([
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      ts.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite
+    ],
+    languageOptions: {
+      globals: globals.browser
+    }
+  }
+]);
