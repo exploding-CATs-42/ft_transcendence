7be8c6db8e7e6a5159f64d2153332060bf97f3e3
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-04-04 21:02:11 +0200

    feat(backend): created endpoints for accessing sockets and rest-api docs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import prettyFormat from "pino-pretty";
 import cors, { CorsOptions } from "cors";
 import { HttpError } from "http-errors";
 import "dotenv/config";
+import { docsRouter } from "./routes/docsRouter/docsRouter";
 
 const { FRONTEND_URL = "*" } = process.env;
 
@@ -21,6 +22,8 @@ app.use(express.json());
 app.get("/", (_, res) => {
   return res.json({ message: "Hello world!" });
 });
+
+app.use("/docs", docsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
