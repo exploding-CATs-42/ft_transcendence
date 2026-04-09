d822c8dd2c34b5133d9863a1aab5fd72c4e02494
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-04-09 12:18:38 +0200

    feat(f): added barrel file to components folder

--- frontend/src/web/components/index.ts
+++ frontend/src/web/components/index.ts
@@ -1,1 +1,8 @@
+export { default as BurgerMenu } from "./BurgerMenu/BurgerMenu";
+export { default as Button } from "./Button/Button";
+export { default as CatImage } from "./CatImage/CatImage";
+export { default as Header } from "./Header/Header";
 export { default as Layout } from "./Layout/Layout";
+export { default as LinkButton } from "./LinkButton/LinkButton";
+export { default as Logo } from "./Logo/Logo";
+export { default as Navigation } from "./Navigation/Navigation";
