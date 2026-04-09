07c88b3d59920e6bae6f3cfc0131e685fb7c3006
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-04-09 12:19:51 +0200

    feat(f): added HomePage page

--- frontend/src/web/pages/HomePage/HomePage.tsx
+++ frontend/src/web/pages/HomePage/HomePage.tsx
@@ -1,7 +1,26 @@
+import { CatImage, LinkButton } from "components";
+
 import s from "./HomePage.module.css";
 
 const HomePage = () => {
-  return <h1>Exploding CATs</h1>;
+  return (
+    <div className={s.pageContainer}>
+      <div className={s.titleButtonsContainer}>
+        <h1 className={s.title}>
+          Exploding <span className={s.kittensSpan}>kittens</span>
+        </h1>
+        <div className={s.buttonsContainer}>
+          <LinkButton className={s.playButton} to={"/lobby"}>
+            Play
+          </LinkButton>
+          <LinkButton className={s.watchButton} to={"/rules"}>
+            Watch Gameplay
+          </LinkButton>
+        </div>
+      </div>
+      <CatImage className={s.catImage} />
+    </div>
+  );
 };
 
 export default HomePage;
