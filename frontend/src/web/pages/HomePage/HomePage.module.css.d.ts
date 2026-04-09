50dd3e08c8e28a0de4cffba11eb77079d40ac77a
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-04-09 12:19:51 +0200

    feat(f): added HomePage page

---
+++ frontend/src/web/pages/HomePage/HomePage.module.css.d.ts
@@ -0,0 +1,12 @@
+declare const styles: {
+  readonly "buttonsContainer": string;
+  readonly "catImage": string;
+  readonly "kittensSpan": string;
+  readonly "pageContainer": string;
+  readonly "playButton": string;
+  readonly "title": string;
+  readonly "titleButtonsContainer": string;
+  readonly "watchButton": string;
+};
+export = styles;
+
