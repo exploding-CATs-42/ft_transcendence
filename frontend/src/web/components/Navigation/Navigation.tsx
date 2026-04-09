0b90832ef8200eebdae101c66bee8f3788a532e7
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-04-09 12:14:42 +0200

    feat(f): added Navigation component

---
+++ frontend/src/web/components/Navigation/Navigation.tsx
@@ -0,0 +1,31 @@
+import { Button, LinkButton } from "components";
+
+import s from "./Navigation.module.css";
+
+const Navigation = () => {
+  const navLinks = [
+    { path: "/lobby", label: "Play" },
+    { path: "/rules", label: "Rules" },
+    { path: "/about", label: "About" }
+  ];
+
+  return (
+    <>
+      <nav>
+        <ul className={s.navList}>
+          {navLinks.map((link) => (
+            <li key={link.path} className={s.navLinkItem}>
+              <LinkButton to={link.path} className={s.link}>
+                {link.label}
+              </LinkButton>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <Button className={s.signUp}>Sign up</Button>
+    </>
+  );
+};
+
+export default Navigation;
