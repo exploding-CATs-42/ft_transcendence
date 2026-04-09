753fc1a91ca72ec9d50d3fb2c58ee2fa147a293b
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-04-09 12:13:55 +0200

    feat(f): added LinkButton component

---
+++ frontend/src/web/components/LinkButton/LinkButton.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+
+import s from "./LinkButton.module.css";
+
+interface Props {
+  className?: string;
+  children?: ReactNode;
+  to: string;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+}
+
+const LinkButton = ({ to, className = "", children }: Props) => {
+  return (
+    <Link className={clsx(s.button, className)} to={to}>
+      {children}
+    </Link>
+  );
+};
+
+export default LinkButton;
