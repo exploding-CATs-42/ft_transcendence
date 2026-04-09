d76dfe775a4c7319b9c3be358be76c0b62a1f3f8
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-04-09 12:16:11 +0200

    feat(f): added BurgerMenu component

---
+++ frontend/src/web/components/BurgerMenu/BurgerMenu.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "burgerMenuButton": string;
+  readonly "burgerMenuContainer": string;
+  readonly "burgerMenuIcon": string;
+  readonly "navContainer": string;
+  readonly "openedBurgerMenu": string;
+};
+export = styles;
+
