dbbf3a099b64929db0476c294489978232573eb3
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-04-10 15:29:58 +0200

    feat: implemented Not Found page

--- frontend/src/web/pages/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/web/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,14 +1,35 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+
+import { LinkButton } from "components";
+import { CatomicBomb } from "assets";
+
 import s from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
+  useEffect(() => {
+    document.body.classList.add(s.background);
+
+    return () => {
+      document.body.classList.remove(s.background);
+    };
+  }, []);
+
   return (
     <div className={s.container}>
-      <h1>404</h1>
-      <h2>Not found</h2>
-      <Link className={s.linkButton} to="/" aria-label="Navigate to Home page">
+      <h1 className="visually-hidden">Not Found</h1>
+      <div className={s.notFoundContainer}>
+        <span className={s.leftFour}>4</span>
+        <img className={s.catomicBomb} src={CatomicBomb} alt="Catomic bomb" />
+        <span className={s.rightFour}>4</span>
+      </div>
+      <p className={s.jokingMessage}>
+        Sorry. This page has exploded. We checked everywhere, but all we found
+        was a confused potato and some suspicious crumbs. You're probably in the
+        wrong place. Try going somewhere less… explody.
+      </p>
+      <LinkButton className={s.linkButton} to="/">
         Home page
-      </Link>
+      </LinkButton>
     </div>
   );
 };
