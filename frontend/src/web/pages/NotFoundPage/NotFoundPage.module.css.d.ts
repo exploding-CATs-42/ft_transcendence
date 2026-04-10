9f4cc7e3a5a6b33de9898082839fb280729a5da8
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-04-10 15:29:58 +0200

    feat: implemented Not Found page

--- frontend/src/web/pages/NotFoundPage/NotFoundPage.module.css.d.ts
+++ frontend/src/web/pages/NotFoundPage/NotFoundPage.module.css.d.ts
@@ -1,6 +1,12 @@
 declare const styles: {
+  readonly "background": string;
+  readonly "catomicBomb": string;
   readonly "container": string;
+  readonly "jokingMessage": string;
+  readonly "leftFour": string;
   readonly "linkButton": string;
+  readonly "notFoundContainer": string;
+  readonly "rightFour": string;
 };
 export = styles;
 
