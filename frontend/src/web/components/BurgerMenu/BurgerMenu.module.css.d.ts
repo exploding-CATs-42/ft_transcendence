227fc89ba2835e81fab8e96af0a6ec542cc7374c
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-04-10 15:26:51 +0200

    feat(f, BurgerMenu): added z-index 1000 to BurgerMenu

--- frontend/src/web/components/BurgerMenu/BurgerMenu.module.css.d.ts
+++ frontend/src/web/components/BurgerMenu/BurgerMenu.module.css.d.ts
@@ -1,7 +1,6 @@
 declare const styles: {
   readonly "burgerMenuButton": string;
   readonly "burgerMenuContainer": string;
-  readonly "burgerMenuIcon": string;
   readonly "navContainer": string;
   readonly "openedBurgerMenu": string;
 };
