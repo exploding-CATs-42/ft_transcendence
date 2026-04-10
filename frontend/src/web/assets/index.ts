0600820e383b9a2cb5eea8056032b48776bb7510
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-04-10 15:29:23 +0200

    chore(f, assets): updated barrel files

--- frontend/src/web/assets/index.ts
+++ frontend/src/web/assets/index.ts
@@ -1,3 +1,2 @@
-export { default as BurgerMenuIcon } from "./icons/burger-menu.svg?react";
-export { default as CrossIcon } from "./icons/cross.svg?react";
-export { default as Logo } from "./icons/logo.svg?react";
+export * from "./images";
+export * from "./icons";
