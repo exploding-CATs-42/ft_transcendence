f8dc4db753778afa04d67d8f55c59b549fbdced8
Andrii Kovtunets <akovtune@student.codam.nl>
2026-04-12 16:15:22 +0200

    feat(f, components): added PasswordInput component

--- frontend/src/web/components/index.ts
+++ frontend/src/web/components/index.ts
@@ -13,4 +13,3 @@ export { default as NameInput } from "./NameInput/NameInput";
 export { default as Avatar } from "./Avatar/Avatar";
 export { default as Navigation } from "./Navigation/Navigation";
 export { default as PasswordInput } from "./PasswordInput/PasswordInput";
-
