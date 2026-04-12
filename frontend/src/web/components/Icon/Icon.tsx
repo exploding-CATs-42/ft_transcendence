e3813b2506ebe3335c0098282332d4044c1bd6bb
Andrii Kovtunets <akovtune@student.codam.nl>
2026-04-12 16:05:47 +0200

    feat(f, components): created Icon component

---
+++ frontend/src/web/components/Icon/Icon.tsx
@@ -0,0 +1,45 @@
+import { Icons } from "assets";
+
+interface Props {
+  width?: number;
+  height?: number;
+  id?: string;
+  className?: string;
+  stroke?: string;
+  fill?: string;
+  name:
+    | "alert"
+    | "checkmark"
+    | "chevron"
+    | "cross"
+    | "error"
+    | "eye"
+    | "eye-off"
+    | "lock"
+    | "logout"
+    | "mail"
+    | "paw"
+    | "pencil"
+    | "plus"
+    | "settings"
+    | "trash-can"
+    | "user"
+    | "burger-menu";
+}
+
+const Icon = ({ name, width, height, id, className, stroke, fill }: Props) => {
+  return (
+    <svg
+      className={className}
+      id={id}
+      width={width}
+      height={height}
+      stroke={stroke}
+      fill={fill}
+    >
+      <use href={`${Icons}#${name}`} />
+    </svg>
+  );
+};
+
+export default Icon;
