16ec93bc2d4f583c9e60a56b79a486924f7de30c
Andrii Kovtunets <akovtune@student.codam.nl>
2026-04-13 16:29:53 +0200

    fix(f): updated CatImage component to use <picture>

--- frontend/src/web/components/CatImage/CatImage.tsx
+++ frontend/src/web/components/CatImage/CatImage.tsx
@@ -4,21 +4,25 @@ interface Props {
 
 const CatImage = ({ className }: Props) => {
   return (
-    <img
-      className={className}
-      src="/src/web/assets/images/cat/cat-mobile.png"
-      srcSet="
-    /src/web/assets/images/cat/cat-mobile.png 328w,
-    /src/web/assets/images/cat/cat-tablet.png 465w,
-    /src/web/assets/images/cat/cat-laptop.png 651w,
-    /src/web/assets/images/cat/cat-desktop.png 708w"
-      sizes="
-    (max-width: 767px) 328px,
-    (max-width: 1439px) 465px,
-    (max-width: 1919px) 651px,
-    708px"
-      alt="Exploding cat hugs Earth"
-    />
+    <picture>
+      <source
+        media="(max-width: 767px)"
+        srcSet="/src/web/assets/images/cat/cat-mobile.png"
+      />
+      <source
+        media="(max-width: 1439px)"
+        srcSet="/src/web/assets/images/cat/cat-tablet.png"
+      />
+      <source
+        media="(max-width: 1919px)"
+        srcSet="/src/web/assets/images/cat/cat-laptop.png"
+      />
+      <img
+        className={className}
+        src="/src/web/assets/images/cat/cat-desktop.png"
+        alt="Exploding cat hugs Earth"
+      />
+    </picture>
   );
 };
 
