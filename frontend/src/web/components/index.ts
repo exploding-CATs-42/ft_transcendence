7e60e78efe01ac17bf427b4a3232d031db19aad9
rmzvr <rmzvr@proton.me>
2026-04-20 14:10:19 +0200

    feat: add List component

--- frontend/src/web/components/index.ts
+++ frontend/src/web/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Avatar } from "./Avatar/Avatar";
 export { default as BurgerMenu } from "./BurgerMenu/BurgerMenu";
 export { default as Button } from "./Button/Button";
 export { default as CatImage } from "./CatImage/CatImage";
@@ -8,10 +9,10 @@ export { default as Icon } from "./Icon/Icon";
 export { default as Input } from "./Input/Input";
 export { default as Layout } from "./Layout/Layout";
 export { default as LinkButton } from "./LinkButton/LinkButton";
+export { default as List } from "./List/List";
 export { default as Logo } from "./Logo/Logo";
 export { default as Modal } from "./Modal/Modal";
 export { default as NameInput } from "./NameInput/NameInput";
-export { default as Avatar } from "./Avatar/Avatar";
 export { default as Navigation } from "./Navigation/Navigation";
 export { default as PasswordInput } from "./PasswordInput/PasswordInput";
 export { default as Section } from "./Section/Section";
