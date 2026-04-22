a28c1c9d22d0139944df6b9c6cc78fd96a50a541
Andrii Kovtunets <akovtune@student.codam.nl>
2026-04-22 14:05:00 +0200

    fix(f, AuthForm): moved Button inside form

--- frontend/src/web/components/AuthForm/AuthForm.tsx
+++ frontend/src/web/components/AuthForm/AuthForm.tsx
@@ -26,15 +26,12 @@ const AuthForm = ({
         <span className={s.title}>{title}</span>
         <Icon name="paw" width={50} height={50} fill={"#fcf8ee"} />
       </div>
-      <form
-        className={s.form}
-        onSubmit={(event) => event.preventDefault()}
-      >
+      <form className={s.form} onSubmit={(event) => event.preventDefault()}>
         <div className={s.inputsContainer}>{children}</div>
+        <Button className={s.signIn} onClick={() => {}}>
+          {title}
+        </Button>
       </form>
-      <Button className={s.signIn} onClick={() => {}}>
-        {title}
-      </Button>
       <span className={s.signUp}>
         {redirectMessage}{" "}
         <Link className={s.signUpLink} to={redirectLink || "/"}>
