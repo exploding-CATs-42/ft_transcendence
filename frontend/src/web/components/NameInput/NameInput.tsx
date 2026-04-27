cde1594b7dec757f5bd2c72a5eb6e8ecfa114b1a
Andrii Kovtunets <akovtune@student.codam.nl>
2026-04-27 15:32:13 +0200

    fix(f, NameInput): removed unnecessary 2-nd css class for icon

--- frontend/src/web/components/NameInput/NameInput.tsx
+++ frontend/src/web/components/NameInput/NameInput.tsx
@@ -1,5 +1,3 @@
-import clsx from "clsx";
-
 import { Icon, Input } from "components";
 
 import s from "./NameInput.module.css";
@@ -13,12 +11,7 @@ interface Props {
 const NameInput = ({ placeholder = "Name", status = "normal" }: Props) => {
   return (
     <Input type="text" pdLeft={true} placeholder={placeholder} status={status}>
-      <Icon
-        className={clsx(s.icon, s.leftIcon)}
-        name="user"
-        width={24}
-        height={24}
-      />
+      <Icon className={s.icon} name="user" width={24} height={24} />
     </Input>
   );
 };
