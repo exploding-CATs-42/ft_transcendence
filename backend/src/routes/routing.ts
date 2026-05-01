b8158382e6242c949ac8574ad142b83ed114a9cd
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-01 15:18:08 +0200

    chore(backend): move express routing into a separate file inside routes folder

---
+++ backend/src/routes/routing.ts
@@ -0,0 +1,29 @@
+import type { Express, Request, Response, NextFunction } from "express";
+import { HttpError } from "http-errors";
+import { docsRouter } from "./docs";
+import { usersRouter } from "./users";
+import { meRouter } from "./me";
+import { friendsRouter } from "./friends";
+
+export const setupRouting = (app: Express) => {
+  app.get("/", (_, res) => {
+    return res.json({ message: "Hello world!" });
+  });
+
+  app.use("/docs", docsRouter);
+  app.use("/users", usersRouter);
+  app.use("/me", meRouter);
+  app.use("/me/friends", friendsRouter);
+
+  app.use((_, res) => {
+    res.status(404).json({ message: "Route not found" });
+  });
+
+  app.use((error: unknown, _: Request, res: Response, __: NextFunction) => {
+    if (error instanceof HttpError)
+      return res.status(error.status).json({ message: error.message });
+
+    console.error("Unhandled error:", error);
+    return res.status(500).json({ message: "Server error" });
+  });
+};
