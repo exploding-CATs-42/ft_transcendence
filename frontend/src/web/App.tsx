20bb78e380d821cb5c1c241f4c2fc06223fe90a9
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-01 15:44:04 +0200

    feat(frontend): add /chat route inside App.tsx

--- frontend/src/web/App.tsx
+++ frontend/src/web/App.tsx
@@ -13,6 +13,7 @@ const GamePage = lazy(() => import("./pages/GamePage/GamePage.jsx"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage.js"));
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
 const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
+const ChatPage = lazy(() => import("./pages/ChatPage/ChatPage"));
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="game" element={<GamePage />}></Route>
         <Route path="login" element={<LoginPage />}></Route>
         <Route path="register" element={<RegisterPage />}></Route>
+        <Route path="chat" element={<ChatPage />}></Route>
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
