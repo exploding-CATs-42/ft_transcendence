bbd7f50937b1a903a7f4d40b9b90301ee0b81795
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-05-06 17:05:48 +0200

    feat(frontend): create FormField component
    FormField component is a wrapper for <input> fields inside a form and is used to display errors.
    It reduces error markup duplication and ensures consistent error message styling.

--- frontend/src/web/components/index.ts
+++ frontend/src/web/components/index.ts
@@ -20,3 +20,4 @@ export { default as PasswordInput } from "./PasswordInput/PasswordInput";
 export { default as SearchInput } from "./SearchInput/SearchInput";
 export { default as Section } from "./Section/Section";
 export { default as PhaserGame } from "./PhaserGame/PhaserGame";
+export { default as FormField } from "./FormField/FormField";
