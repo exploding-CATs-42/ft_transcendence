2c4e0ce97129aa596c3994848d13894ccadefdf2
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-05-07 22:26:06 +0200

    fix(frontend): spread remaining props to Input element inside NameInput, EmailInput and PasswordInput

--- frontend/src/web/components/EmailInput/EmailInput.tsx
+++ frontend/src/web/components/EmailInput/EmailInput.tsx
@@ -7,9 +7,15 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   status?: InputStatus;
 }
 
-const EmailInput = ({ placeholder = "Email", status }: Props) => {
+const EmailInput = ({ placeholder = "Email", status, ...rest }: Props) => {
   return (
-    <Input type="email" pdLeft={true} placeholder={placeholder} status={status}>
+    <Input
+      {...rest}
+      type="email"
+      pdLeft={true}
+      placeholder={placeholder}
+      status={status}
+    >
       <Icon className={s.icon} name="mail" width={24} height={24} />
     </Input>
   );
--- frontend/src/web/components/NameInput/NameInput.tsx
+++ frontend/src/web/components/NameInput/NameInput.tsx
@@ -7,9 +7,15 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   status?: InputStatus;
 }
 
-const NameInput = ({ placeholder = "Name", status }: Props) => {
+const NameInput = ({ placeholder = "Name", status, ...rest }: Props) => {
   return (
-    <Input type="text" pdLeft={true} placeholder={placeholder} status={status}>
+    <Input
+      {...rest}
+      type="text"
+      pdLeft={true}
+      placeholder={placeholder}
+      status={status}
+    >
       <Icon className={s.icon} name="user" width={24} height={24} />
     </Input>
   );
