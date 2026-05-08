bedda4035613e41420fb85dfa2c84357bebd6bbf
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-05-08 23:15:37 +0200

    feat(frontend): add react-toastify ToastContainer with default configuration inside Layout
    It's being done to enable global toast notifications across the app.

--- frontend/src/web/components/Layout/Layout.tsx
+++ frontend/src/web/components/Layout/Layout.tsx
@@ -1,13 +1,22 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer, type ToastContainerProps } from "react-toastify";
+
 import { Header } from "components";
 
 const Layout = () => {
+  const toastConfig: ToastContainerProps = {
+    theme: "light",
+    autoClose: 2000,
+    style: { zIndex: 99999 }
+  };
+
   return (
     <>
       <Header />
       <main>
         <Outlet />
       </main>
+      <ToastContainer {...toastConfig} />
     </>
   );
 };
