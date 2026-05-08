1ce17ca837b1f698275c72c0e5bd7b5860a58a35
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-05-08 19:36:43 +0200

    fix(frontend): rename VITE_BASE_URL into VITE_API_BASE_URL inside axios client

--- frontend/src/web/api/axios.ts
+++ frontend/src/web/api/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const { VITE_BASE_URL } = import.meta.env;
+const { VITE_API_BASE_URL } = import.meta.env;
 
 export const api = axios.create({
-  baseURL: VITE_BASE_URL
+  baseURL: VITE_API_BASE_URL
 });
 
 export const setAxiosToken = (token: string) => {
