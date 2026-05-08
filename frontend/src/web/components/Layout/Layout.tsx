b831b3c78df0e9f73c4a1d6c23e77280f8ca6923
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-05-08 23:29:46 +0200

    feat(frontend): use useToastPosition hook inside Layout

--- frontend/src/web/components/Layout/Layout.tsx
+++ frontend/src/web/components/Layout/Layout.tsx
@@ -2,12 +2,16 @@ import { Outlet } from "react-router-dom";
 import { ToastContainer, type ToastContainerProps } from "react-toastify";
 
 import { Header } from "components";
+import { useToastPosition } from "hooks";
 
 const Layout = () => {
+  const toastPosition = useToastPosition();
+
   const toastConfig: ToastContainerProps = {
     theme: "light",
     autoClose: 2000,
-    style: { zIndex: 99999 }
+    style: { zIndex: 99999 },
+    position: toastPosition
   };
 
   return (
