f8c30f1bea621a10e20501780d1b835ed8432f72
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-05-09 16:16:41 +0200

    feat(frontend): add react-hook-form validation with zod to AuthForm inside RegisterPage

--- frontend/src/web/pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/web/pages/RegisterPage/RegisterPage.tsx
@@ -1,8 +1,25 @@
+// Libraries
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+// Project level
 import { AuthForm, EmailInput, NameInput, PasswordInput } from "components";
-
+import { registerSchema, type RegisterSchema } from "schemas";
+// Local level
 import s from "./RegisterPage.module.css";
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema)
+  });
+
+  const onSubmit: SubmitHandler<RegisterSchema> = async (data) => {
+    console.log(data);
+  };
+
   return (
     <div className={s.backgroundContainer}>
       <div className={s.blur} />
@@ -16,11 +33,26 @@ const RegisterPage = () => {
             redirectMessage="Already have an account?"
             redirectTitle="Sign in"
             redirectLink="/login"
+            onSubmit={handleSubmit(onSubmit)}
           >
-            <EmailInput></EmailInput>
-            <NameInput placeholder="Username"></NameInput>
-            <PasswordInput></PasswordInput>
-            <PasswordInput placeholder="Password confirmation"></PasswordInput>
+            <EmailInput
+              {...register("email")}
+              status={errors.email ? "error" : "normal"}
+            />
+            <NameInput
+              {...register("username")}
+              status={errors.username ? "error" : "normal"}
+              placeholder="Username"
+            />
+            <PasswordInput
+              {...register("password")}
+              status={errors.password ? "error" : "normal"}
+            />
+            <PasswordInput
+              {...register("passwordConfirm")}
+              status={errors.passwordConfirm ? "error" : "normal"}
+              placeholder="Password confirmation"
+            />
           </AuthForm>
         </div>
       </div>
