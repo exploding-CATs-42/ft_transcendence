58951d1db7941306abaa953a5af7d6888a75d8d0
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-05-12 11:55:18 +0200

    refactor(backend): move socket middleware in a separate function

---
+++ backend/src/middlewares/socketMiddleware.ts
@@ -0,0 +1,23 @@
+import { verifyAccessToken } from "../utils/jwt";
+import { ExtendedError, Socket } from "socket.io";
+
+export function socketMiddleware(
+  socket: Socket,
+  next: (err?: ExtendedError | undefined) => void
+) {
+  try {
+    const token = socket.handshake.auth["token"];
+
+    if (!token) {
+      return next(new Error("Authentication token missing"));
+    }
+
+    const payload = verifyAccessToken(token);
+
+    socket.data = payload;
+
+    next();
+  } catch (_) {
+    next(new Error("Invalid token"));
+  }
+}
