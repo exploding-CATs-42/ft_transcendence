870484e3afbb01acf5533032eb6bc763a770a00b
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-15 12:39:04 +0200

    refactor(frontend): switch PhaserGame forwardRef component from named to arrow function

--- frontend/src/web/components/PhaserGame/PhaserGame.tsx
+++ frontend/src/web/components/PhaserGame/PhaserGame.tsx
@@ -9,51 +9,50 @@ interface IProps {
   currentActiveScene?: (scene_instance: Phaser.Scene) => void;
 }
 
-const PhaserGame = forwardRef<IRefPhaserGame, IProps>(function PhaserGame(
-  { currentActiveScene },
-  ref,
-) {
-  const game = useRef<Phaser.Game | null>(null!);
+const PhaserGame = forwardRef<IRefPhaserGame, IProps>(
+  ({ currentActiveScene }, ref) => {
+    const game = useRef<Phaser.Game | null>(null!);
 
-  useLayoutEffect(() => {
-    if (game.current === null) {
-      game.current = StartGame("game-container");
+    useLayoutEffect(() => {
+      if (game.current === null) {
+        game.current = StartGame("game-container");
 
-      if (typeof ref === "function") {
-        ref({ game: game.current, scene: null });
-      } else if (ref) {
-        ref.current = { game: game.current, scene: null };
-      }
-    }
-
-    return () => {
-      if (game.current) {
-        game.current.destroy(true);
-        if (game.current !== null) {
-          game.current = null;
+        if (typeof ref === "function") {
+          ref({ game: game.current, scene: null });
+        } else if (ref) {
+          ref.current = { game: game.current, scene: null };
         }
       }
-    };
-  }, [ref]);
 
-  useEffect(() => {
-    EventBus.on("current-scene-ready", (scene_instance: Phaser.Scene) => {
-      if (currentActiveScene && typeof currentActiveScene === "function") {
-        currentActiveScene(scene_instance);
-      }
+      return () => {
+        if (game.current) {
+          game.current.destroy(true);
+          if (game.current !== null) {
+            game.current = null;
+          }
+        }
+      };
+    }, [ref]);
 
-      if (typeof ref === "function") {
-        ref({ game: game.current, scene: scene_instance });
-      } else if (ref) {
-        ref.current = { game: game.current, scene: scene_instance };
-      }
-    });
-    return () => {
-      EventBus.removeListener("current-scene-ready");
-    };
-  }, [currentActiveScene, ref]);
+    useEffect(() => {
+      EventBus.on("current-scene-ready", (scene_instance: Phaser.Scene) => {
+        if (currentActiveScene && typeof currentActiveScene === "function") {
+          currentActiveScene(scene_instance);
+        }
 
-  return <div id="game-container"></div>;
-});
+        if (typeof ref === "function") {
+          ref({ game: game.current, scene: scene_instance });
+        } else if (ref) {
+          ref.current = { game: game.current, scene: scene_instance };
+        }
+      });
+      return () => {
+        EventBus.removeListener("current-scene-ready");
+      };
+    }, [currentActiveScene, ref]);
+
+    return <div id="game-container"></div>;
+  },
+);
 
 export default PhaserGame;
