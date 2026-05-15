d4abf750cadc8557cb05af1e8ff7678959e76f83
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-15 18:22:53 +0200

    refactor(frontend): simplify code inside GamePage

--- frontend/src/web/pages/GamePage/GamePage.tsx
+++ frontend/src/web/pages/GamePage/GamePage.tsx
@@ -10,58 +10,51 @@ import s from "./GamePage.module.css";
 const GamePage = () => {
   // The sprite can only be moved in the MainMenu Scene
   const [canMoveSprite, setCanMoveSprite] = useState(true);
-
-  //  References to the PhaserGame component (game and scene are exposed)
-  const phaserRef = useRef<PhaserGameRef | null>(null);
   const [spritePosition, setSpritePosition] = useState({ x: 0, y: 0 });
 
-  const changeScene = () => {
-    if (phaserRef.current) {
-      const scene = phaserRef.current.scene as MainMenu;
+  //  References to the PhaserGame component (game and scene are exposed)
+  const phaserRef = useRef<PhaserGameRef>(null);
 
-      if (scene) {
-        scene.changeScene();
-      }
-    }
+  const getScene = () => phaserRef.current?.scene ?? null;
+
+  const changeScene = () => {
+    (getScene() as MainMenu)?.changeScene();
   };
 
   const moveSprite = () => {
-    if (phaserRef.current) {
-      const scene = phaserRef.current.scene as MainMenu;
+    const scene = getScene() as MainMenu;
 
-      if (scene && scene.scene.key === "MainMenu") {
-        // Get the update logo position
-        scene.moveLogo(({ x, y }) => {
-          setSpritePosition({ x, y });
-        });
-      }
-    }
+    if (!scene) return;
+    if (scene.scene.key !== "MainMenu") return;
+
+    // Get the update logo position
+    scene.moveLogo(({ x, y }) => {
+      setSpritePosition({ x, y });
+    });
   };
 
   const addSprite = () => {
-    if (phaserRef.current) {
-      const scene = phaserRef.current.scene;
+    const scene = getScene();
 
-      if (scene) {
-        // Add more stars
-        const x = Phaser.Math.Between(64, scene.scale.width - 64);
-        const y = Phaser.Math.Between(64, scene.scale.height - 64);
+    if (!scene) return;
 
-        //  `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
-        const star = scene.add.sprite(x, y, "star");
+    // Add more stars
+    const x = Phaser.Math.Between(64, scene.scale.width - 64);
+    const y = Phaser.Math.Between(64, scene.scale.height - 64);
 
-        //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
-        //  You could, of course, do this from within the Phaser Scene code, but this is just an example
-        //  showing that Phaser objects and systems can be acted upon from outside of Phaser itself.
-        scene.add.tween({
-          targets: star,
-          duration: 500 + Math.random() * 1000,
-          alpha: 0,
-          yoyo: true,
-          repeat: -1,
-        });
-      }
-    }
+    //  `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
+    const star = scene.add.sprite(x, y, "star");
+
+    //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
+    //  You could, of course, do this from within the Phaser Scene code, but this is just an example
+    //  showing that Phaser objects and systems can be acted upon from outside of Phaser itself.
+    scene.add.tween({
+      targets: star,
+      duration: 500 + Math.random() * 1000,
+      alpha: 0,
+      yoyo: true,
+      repeat: -1,
+    });
   };
 
   // Event emitted from the PhaserGame component
