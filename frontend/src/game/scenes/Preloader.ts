21accf17b4dce55feb24d7656a870414209f8c53
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-16 21:02:40 +0200

    feat(frontend): replace Game scene with WaitingRoom scene inside Preloader scene

--- frontend/src/game/scenes/Preloader.ts
+++ frontend/src/game/scenes/Preloader.ts
@@ -38,6 +38,6 @@ export class Preloader extends Scene {
     //  For example, you can define global animations here, so we can use them in other scenes.
 
     //  Move to the Game scene. You could also swap this for a Scene Transition, such as a camera fade.
-    this.scene.start(Scenes.Game);
+    this.scene.start(Scenes.WaitingRoom);
   }
 }
