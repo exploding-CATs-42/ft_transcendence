af0aebcc6c02c50fcdaa2a061b6d0c610295e09b
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-16 20:27:59 +0200

    feat(frontend): replace MainMenu scene with Game scene inside Preloader scene

--- frontend/src/game/scenes/Preloader.ts
+++ frontend/src/game/scenes/Preloader.ts
@@ -35,7 +35,7 @@ export class Preloader extends Scene {
     //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
     //  For example, you can define global animations here, so we can use them in other scenes.
 
-    //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-    this.scene.start("MainMenu");
+    //  Move to the Game scene. You could also swap this for a Scene Transition, such as a camera fade.
+    this.scene.start("Game");
   }
 }
