bbdfac6765c19ff7393b9b1132b64dea5f6e506e
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-16 18:14:22 +0200

    easterEgg: Making your first Phaser Game

--- frontend/src/game/main.ts
+++ frontend/src/game/main.ts
@@ -1,18 +1,20 @@
-import { Boot } from "./scenes/Boot";
-import { GameOver } from "./scenes/GameOver";
-import { Game as MainGame } from "./scenes/Game";
-import { MainMenu } from "./scenes/MainMenu";
 import { AUTO, Game } from "phaser";
-import { Preloader } from "./scenes/Preloader";
+import { WaitingRoom } from "./scenes";
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
   type: AUTO,
-  width: 1024,
-  height: 768,
-  backgroundColor: "#028af8",
-  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+  width: 800,
+  height: 600,
+  scene: [WaitingRoom],
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300, x: 0 },
+      debug: false,
+    },
+  },
 };
 
 const StartGame = (parent: HTMLDivElement) => {
