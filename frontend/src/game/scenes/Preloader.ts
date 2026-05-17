14a1df3e7674ecf6fe7030d56d37f2b7908e789b
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-17 17:16:11 +0200

    refactor(frontend): refactor Preloader scene

--- frontend/src/game/scenes/Preloader.ts
+++ frontend/src/game/scenes/Preloader.ts
@@ -11,25 +11,12 @@ export class Preloader extends Scene {
 
   init() {
     //  We loaded this image in our Boot Scene, so we can display it here
-    this.add.image(0, 0, Textures.background).setOrigin(0, 0);
-
-    //  A simple progress bar. This is the outline of the bar.
-    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
-
-    //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
-
-    //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-    this.load.on("progress", (progress: number) => {
-      //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-      bar.width = 4 + 460 * progress;
-    });
+    this.addBackgroundImage(Textures.background);
+    this.addProgressBar();
   }
 
   preload() {
     //  Load the assets for the game - Replace with your own assets
-    this.load.setPath("src/game/assets/images");
-
     this.load.image(Textures.avatar, avatar);
   }
 
@@ -39,5 +26,57 @@ export class Preloader extends Scene {
 
     //  Move to the Game scene. You could also swap this for a Scene Transition, such as a camera fade.
     this.scene.start(Scenes.WaitingRoom);
+  }
+
+  private addBackgroundImage(texture: string) {
+    const bg = this.add.image(
+      this.scale.width / 2,
+      this.scale.height / 2,
+      texture,
+    );
+
+    const scaleX = this.scale.width / bg.width;
+    const scaleY = this.scale.height / bg.height;
+    const scale = Math.max(scaleX, scaleY);
+
+    bg.setOrigin(0.5);
+    bg.setScale(scale);
+
+    return bg;
+  }
+
+  private addProgressBar() {
+    //  A simple progress bar
+    const progressBarWidth = 468;
+    const progressBarHeight = 32;
+    const marginBottom = 100;
+
+    // Center horizontally
+    const x = this.scale.width / 2;
+
+    // Position from bottom
+    const y = this.scale.height - marginBottom;
+
+    // Progress bar outline
+    this.add
+      .rectangle(x, y, progressBarWidth, progressBarHeight)
+      .setStrokeStyle(1, 0xffffff);
+
+    // This is the progress bar itself. It will increase in size from the left based on the % of progress.
+    const bar = this.add.rectangle(
+      x - progressBarWidth / 2 + 2,
+      y,
+      4,
+      progressBarHeight - 4,
+      0xffffff,
+    );
+
+    // Anchor from the left side
+    bar.setOrigin(0, 0.5);
+
+    //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
+    this.load.on("progress", (progress: number) => {
+      bar.width = (progressBarWidth - 4) * progress;
+    });
   }
 }
