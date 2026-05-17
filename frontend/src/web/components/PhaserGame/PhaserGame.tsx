91e002eb9d5724737663b37d2aff0e6546cf1e1a
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-17 16:09:15 +0200

    feat(frontend): make Phaser canvas fit into page

--- frontend/src/web/components/PhaserGame/PhaserGame.tsx
+++ frontend/src/web/components/PhaserGame/PhaserGame.tsx
@@ -53,7 +53,7 @@ const PhaserGame = ({ currentActiveScene, ref }: Props) => {
     };
   }, [currentActiveScene]);
 
-  return <div ref={containerRef} />;
+  return <div ref={containerRef} style={{ width: "100%", height: "100%" }} />;
 };
 
 export default PhaserGame;
--- frontend/src/web/pages/GamePage/GamePage.module.css.d.ts
+++ frontend/src/web/pages/GamePage/GamePage.module.css.d.ts
@@ -1,7 +1,1 @@
-declare const styles: {
-  readonly "app": string;
-  readonly "button": string;
-  readonly "spritePosition": string;
-};
-export = styles;
-
+export {};
