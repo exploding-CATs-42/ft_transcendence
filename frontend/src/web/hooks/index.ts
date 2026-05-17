4cb96abc487394a042365b22a4cc32221dfd2212
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-05-17 21:14:28 +0200

    feat(frontend): add useAuth hook

--- frontend/src/web/hooks/index.ts
+++ frontend/src/web/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useModal } from "./modal/useModal";
 export { useLocalStorage } from "./storage/useLocalStorage";
 export { useToastPosition } from "./toastify/useToastPosition";
+export { useAuth } from "./auth/useAuth";
