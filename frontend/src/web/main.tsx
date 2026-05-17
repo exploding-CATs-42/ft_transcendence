f141f2e9ec3d6ddead213869b2cecaa5a1b8543f
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-05-17 21:14:51 +0200

    feat(frontend): wrap App in AuthProvider inside main.tsx

--- frontend/src/web/main.tsx
+++ frontend/src/web/main.tsx
@@ -1,10 +1,15 @@
+// Libraries
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+// Project level
+import { AuthProvider } from "context";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
 );
