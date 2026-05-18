7039c64a7a640bb598cbaeab1c0699765b65c55d
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-18 16:48:07 +0200

    assets(frontend): add waiting room background png asset

--- frontend/src/game/assets/images/index.ts
+++ frontend/src/game/assets/images/index.ts
@@ -1,2 +1,3 @@
 export { default as background } from "./background.png";
 export { default as avatar } from "./avatar.png";
+export { default as waitingRoomBg } from "./waiting_room_background.png";
