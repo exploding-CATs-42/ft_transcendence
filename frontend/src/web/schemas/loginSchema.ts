0037408d9e4e6ca6713230fc323dc372433de8a9
YellowFlash1040 <andrejkovtunec@gmail.com>
2026-05-18 11:53:29 +0200

    style: fix formatting errors

--- frontend/src/web/schemas/loginSchema.ts
+++ frontend/src/web/schemas/loginSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.email("Invalid email address"),
-  password: z.string()
+  password: z.string(),
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
