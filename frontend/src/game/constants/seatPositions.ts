c8b84d13aa0bf60f229cdeb0bd34bd76a87d51c7
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-18 20:56:30 +0200

    feat(frontend): add SEATS constant that stores player seat position coordinates on the screen

---
+++ frontend/src/game/constants/seatPositions.ts
@@ -0,0 +1,9 @@
+import type { Point } from "game/@types";
+
+export const SEATS: Point[] = [
+  { x: 44, y: 700 },
+  { x: 120, y: 200 },
+  { x: 602, y: 90 },
+  { x: 1098, y: 90 },
+  { x: 1600, y: 180 },
+];
