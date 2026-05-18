8b3b3e4b904af1fb6e5927dfdd215351669bf1b3
Ivan Riadynskyi <iriadyns@f1r3s15.codam.nl>
2026-05-18 18:06:44 +0200

    chore(frontend): add create table modal assets

--- frontend/src/web/assets/images/index.ts
+++ frontend/src/web/assets/images/index.ts
@@ -1,1 +1,5 @@
+export { default as FireCatBackground } from "./background/fire-cat.png";
 export { default as CatomicBomb } from "./catomic-bomb/catomic-bomb.png";
+export { default as CreateTableButton } from "./create-table-modal/button.png";
+export { default as CreateTableNameDefault } from "./create-table-modal/name-default.png";
+export { default as CreateTableTitle } from "./create-table-modal/title.png";
