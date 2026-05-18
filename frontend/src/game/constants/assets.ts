0f06dd52c3c15acf1c07c4e951a043d41e5dd95b
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-18 16:49:46 +0200

    feat(frontend): add key for the waiting room background image asset

--- frontend/src/game/constants/assets.ts
+++ frontend/src/game/constants/assets.ts
@@ -1,6 +1,7 @@
 export const Textures = {
   background: "background",
   avatar: "avatar",
+  waitingRoomBg: "waitingRoomBg",
 } as const;
 
 type Textures = (typeof Textures)[keyof typeof Textures];
