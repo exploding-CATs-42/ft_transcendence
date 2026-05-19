ec365d499db67ff2651b479b4009a4f5005ca1d2
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-05-19 21:07:52 +0200

    refactor(backend): improve schema definitions for consistency and clarity

--- backend/src/middlewares/lobby/ErrorRequestMiddleware.ts
+++ backend/src/middlewares/lobby/ErrorRequestMiddleware.ts
@@ -4,7 +4,8 @@ import { ApiError } from "../../errors/lobby/apiError";
 export const errorMiddleware: ErrorRequestHandler = (error, _, res, __) => {
   if (error instanceof ApiError) {
     res.status(error.statusCode).json({
-      message: error.message
+      message: error.message,
+      details: error.details
     });
     return;
   }
--- backend/src/schemas/games/createGameSchema.ts
+++ backend/src/schemas/games/createGameSchema.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 
-export const createGameSchema = z.object({
-  gameName: z.string().trim().min(3).max(30),
-  maxPlayers: z.coerce.number().min(2).max(5)
-});
+export const createGameSchema = z
+  .object({
+    gameName: z.string().trim().min(3).max(30),
+    maxPlayers: z.coerce.number().min(2).max(5)
+  })
+  .strict();
 
 export type CreateGameRequestBody = z.infer<typeof createGameSchema>;
