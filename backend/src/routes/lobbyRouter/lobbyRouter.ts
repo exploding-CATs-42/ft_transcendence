e2591e554a6f8fa44a038e5541c65f5ac963c27b
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-05-19 21:07:52 +0200

    refactor(backend): remove unused authMiddleware from lobbyRouter

--- backend/src/routes/lobbyRouter/lobbyRouter.ts
+++ backend/src/routes/lobbyRouter/lobbyRouter.ts
@@ -4,14 +4,11 @@ import {
   getGameByIdController,
   getGamesController
 } from "../../controllers/gamesController";
-import { authMiddleware } from "../../middlewares/authMiddleware";
 import { errorMiddleware } from "../../middlewares/lobby/ErrorRequestMiddleware";
 import { asyncHandler } from "../../utils/asyncHandler";
 import { createAuthenticatedRouter } from "./createAuthenticatedRouter";
 
 export const lobbyRouter = createAuthenticatedRouter();
-
-lobbyRouter.use(authMiddleware);
 
 lobbyRouter.get("/", asyncHandler(getGamesController));
 lobbyRouter.get("/:gameId", asyncHandler(getGameByIdController));
