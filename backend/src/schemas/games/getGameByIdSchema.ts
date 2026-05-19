75fc2468d85969dcaa39e1f98adad14a57e31d2c
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-05-19 21:07:52 +0200

    refactor(backend): improve schema definitions for consistency and clarity

--- backend/src/schemas/games/getGameByIdSchema.ts
+++ backend/src/schemas/games/getGameByIdSchema.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 import { gameId } from "./ids.schema";
 
-export const getGameByIdParamsSchema = z.object({
-  gameId
-});
+export const getGameByIdParamsSchema = z
+  .object({
+    gameId
+  })
+  .strict();
 
 export type GetGameByIdParams = z.infer<typeof getGameByIdParamsSchema>;
