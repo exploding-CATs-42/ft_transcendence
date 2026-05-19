940e976aaf56d423614511004fba23b23218f2f6
Ivan Riadynskyi <iriadyns@f1r3s15.codam.nl>
2026-05-19 12:40:20 +0200

    chore(frontend): move fire cat image asset

--- frontend/src/web/assets/images/index.ts
+++ frontend/src/web/assets/images/index.ts
@@ -1,2 +1,2 @@
-export { default as FireCatBackground } from "./background/fire-cat.png";
 export { default as CatomicBomb } from "./catomic-bomb/catomic-bomb.png";
+export { default as FireCat } from "./fire-cat/fire-cat.png";
