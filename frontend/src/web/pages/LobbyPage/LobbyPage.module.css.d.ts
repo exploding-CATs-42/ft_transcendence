98ba27247170e09c0a77570e5be8aa2fe0787994
osivkov <sivkovfollow@gmail.com>
2026-05-20 17:57:38 +0200

    feat(lobby): add join table modal UI

--- frontend/src/web/pages/LobbyPage/LobbyPage.module.css.d.ts
+++ frontend/src/web/pages/LobbyPage/LobbyPage.module.css.d.ts
@@ -5,6 +5,12 @@ declare const styles: {
   readonly "list": string;
   readonly "listSection": string;
   readonly "pageContainer": string;
+  readonly "joinModal": string;
+  readonly "modalTitle": string;
+  readonly "modalLabel": string;
+  readonly "modalInput": string;
+  readonly "modalButtons": string;
+  readonly "modalButton": string;
 };
 export = styles;
 
