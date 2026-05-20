201759ab6c34a18787e78449d2424e1def793630
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-20 17:21:06 +0200

    feat(frontend): add "routes" alias to tsconfig and vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,11 +17,12 @@ export default defineConfig({
       api: resolve(__dirname, "src/web/api"),
       schemas: resolve(__dirname, "src/web/schemas"),
       constants: resolve(__dirname, "src/web/constants"),
-      context: resolve(__dirname, "src/web/context")
-    }
+      context: resolve(__dirname, "src/web/context"),
+      routes: resolve(__dirname, "src/web/routes"),
+    },
   },
   server: {
     host: "0.0.0.0",
-    port: 5173
-  }
+    port: 5173,
+  },
 });
