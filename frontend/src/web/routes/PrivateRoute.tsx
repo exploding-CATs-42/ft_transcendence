eed260eb602402a32f7eb2362b77d8e884ef1cc8
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-20 16:37:29 +0200

    feat(frontend): uncomment Private/PublicRoute.tsx files and make them use useAuth() hook

--- frontend/src/web/routes/PrivateRoute.tsx
+++ frontend/src/web/routes/PrivateRoute.tsx
@@ -1,22 +1,22 @@
-// import { Navigate, useLocation } from "react-router-dom";
-// import type{ ReactNode } from "react";
+// Libraries
+import { Navigate, useLocation } from "react-router-dom";
+import type { ReactNode } from "react";
+// Project level
+import { useAuth } from "hooks";
 
-// import { useAppSelector } from "../hooks";
-// import { selectIsLoggedIn } from "../redux";
+interface Props {
+  children: ReactNode;
+}
 
-// interface Props {
-//   children: ReactNode;
-// }
+const PrivateRoute = ({ children }: Props) => {
+  const { accessToken: isLoggedIn } = useAuth();
+  const location = useLocation();
 
-// const PrivateRoute = ({ children }: Props) => {
-//   const isLoggedIn = useAppSelector(selectIsLoggedIn);
-//   const location = useLocation();
+  return isLoggedIn ? (
+    children
+  ) : (
+    <Navigate state={{ from: location }} to={"/"} />
+  );
+};
 
-//   return isLoggedIn ? (
-//     children
-//   ) : (
-//     <Navigate state={{ from: location }} to={"/"} />
-//   );
-// };
-
-// export default PrivateRoute;
+export default PrivateRoute;
