5916b3d6286fa520e2558d6020f7c998a3c17590
rmzvr <rmzvr@proton.me>
2026-05-21 23:13:56 +0200

    feat(backend): add JOIN_GAME event with addPlayer action to gameMachine

--- backend/src/game/events.ts
+++ backend/src/game/events.ts
@@ -1,7 +1,12 @@
+import { Player } from "./types";
+
 export const GameEventType = {
   START_GAME: "START_GAME",
+  JOIN_GAME: "JOIN_GAME",
 } as const;
 
 export type GameEventType = (typeof GameEventType)[keyof typeof GameEventType];
 
-export type GameEvent = { type: typeof GameEventType.START_GAME };
+export type GameEvent =
+  | { type: typeof GameEventType.START_GAME }
+  | { type: typeof GameEventType.JOIN_GAME; player: Player };
