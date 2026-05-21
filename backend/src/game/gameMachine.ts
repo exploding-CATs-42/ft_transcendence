df2170624bc8c72f90aed0c44d199109dde97088
rmzvr <rmzvr@proton.me>
2026-05-21 23:11:20 +0200

    feat(backend): define initial gameMachine context

--- backend/src/game/gameMachine.ts
+++ backend/src/game/gameMachine.ts
@@ -1,13 +1,21 @@
 import { setup } from "xstate";
+import { Player } from "./types";
+
+export interface GameContext {
+  players: Player[];
+}
 
 export const gameMachine = setup({
   types: {
-    context: {},
+    context: {} as GameContext,
   },
   actions: {},
 }).createMachine({
   id: "game",
   initial: "waiting",
+  context: () => ({
+    players: [],
+  }),
   states: {
     waiting: {},
     playing: {},
