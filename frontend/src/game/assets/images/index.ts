bc5ceb98bbf1360fc6fda13e98f9d0097eb3cc7d
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-21 15:33:39 +0200

    refactor(frontend): rename "background" into "preloaderBg"

--- frontend/src/game/assets/images/index.ts
+++ frontend/src/game/assets/images/index.ts
@@ -1,4 +1,4 @@
-export { default as background } from "./background.png";
+export { default as preloaderBg } from "./preloader_background.png";
 export { default as avatar } from "./avatar.png";
 export { default as waitingRoomBg } from "./waiting_room_background.png";
 export { default as gameRoomBg } from "./game_room_background.png";
--- frontend/src/game/scenes/Boot.ts
+++ frontend/src/game/scenes/Boot.ts
@@ -1,7 +1,7 @@
 // Libraries
 import { Scene } from "phaser";
 // Project level
-import { background } from "game/assets";
+import { preloaderBg } from "game/assets";
 import { Scenes, Textures } from "game/constants";
 
 export class Boot extends Scene {
@@ -12,7 +12,7 @@ export class Boot extends Scene {
   preload() {
     //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
     //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
-    this.load.image(Textures.background, background);
+    this.load.image(Textures.preloaderBg, preloaderBg);
   }
 
   create() {
