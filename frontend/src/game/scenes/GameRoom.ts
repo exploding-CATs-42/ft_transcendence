1ec3037d6677f2bc34f034ae689554d12e37f7a7
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-21 15:46:28 +0200

    feat(frontend): use addPlayers utility function inside GameRoom scene with data placeholder

--- frontend/src/game/scenes/GameRoom.ts
+++ frontend/src/game/scenes/GameRoom.ts
@@ -2,7 +2,24 @@
 import { Scene } from "phaser";
 // Project level
 import { Scenes, Textures } from "game/constants";
-import { EventBus, addBackgroundImage, addFullscreenToggle } from "game/utils";
+import {
+  EventBus,
+  addBackgroundImage,
+  addFullscreenToggle,
+  addPlayers,
+} from "game/utils";
+import type { Player } from "game/entities";
+
+// It's just a placeholder and has to be removed later
+const data: { players: Player[] } = {
+  players: [
+    { username: "You", imageUrl: null },
+    { username: "Player 2", imageUrl: null },
+    { username: "Player 3", imageUrl: null },
+    { username: "Player 4", imageUrl: null },
+    { username: "Player 5", imageUrl: null },
+  ],
+};
 
 export class GameRoom extends Scene {
   constructor() {
@@ -12,6 +29,7 @@ export class GameRoom extends Scene {
   create() {
     addBackgroundImage(this, Textures.gameRoomBg);
     addFullscreenToggle(this);
+    addPlayers(this, data.players, "white");
     EventBus.emit("current-scene-ready", this);
   }
 }
