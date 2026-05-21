03c61438de34e09500fdc0f1081ff342ddce2edd
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-21 15:44:03 +0200

    feat(frontend): use addPlayers utility function inside WaitingRoom scene

--- frontend/src/game/scenes/WaitingRoom.ts
+++ frontend/src/game/scenes/WaitingRoom.ts
@@ -1,8 +1,12 @@
 // Libraries
 import { Scene } from "phaser";
 // Project level
-import { Scenes, Textures, SEATS } from "game/constants";
-import { addBackgroundImage, addFullscreenToggle } from "game/utils";
+import { Scenes, Textures } from "game/constants";
+import {
+  addBackgroundImage,
+  addFullscreenToggle,
+  addPlayers,
+} from "game/utils";
 import type { Player } from "game/entities";
 
 // It's just a placeholder and has to be removed later
@@ -30,33 +34,8 @@ export class WaitingRoom extends Scene {
     addBackgroundImage(this, Textures.waitingRoomBg);
     addFullscreenToggle(this);
 
-    this.addPlayers(data.players);
+    addPlayers(this, data.players, "black");
     this.addWaitingLabel();
-  }
-
-  private addPlayers(players: Player[]) {
-    players.forEach((player, i) => {
-      const { x, y } = SEATS[i]!;
-      const avatar = this.add.image(x, y, Textures.avatar).setOrigin(0, 0);
-
-      const radius = Math.min(avatar.displayWidth, avatar.displayHeight) / 2;
-      const cx = x + radius;
-      const cy = y + radius;
-
-      avatar.setPosition(cx, cy).setOrigin(0.5, 0.5);
-
-      const mask = this.add.graphics();
-      mask.fillCircle(cx, cy, radius);
-      avatar.setMask(mask.createGeometryMask());
-
-      this.add
-        .text(cx, cy - radius - 30, player.username, {
-          fontSize: 32,
-          color: "black",
-          fontFamily: "Chewy",
-        })
-        .setOrigin(0.5, 0.5);
-    });
   }
 
   private addWaitingLabel() {
