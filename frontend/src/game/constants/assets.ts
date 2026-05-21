fa3a33899b0ef337ecb2c9fe99acd1d64d065d39
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-21 15:08:21 +0200

    assets(frontend): add game_room_background.png file

--- frontend/src/game/constants/assets.ts
+++ frontend/src/game/constants/assets.ts
@@ -2,6 +2,7 @@ export const Textures = {
   background: "background",
   avatar: "avatar",
   waitingRoomBg: "waitingRoomBg",
+  gameRoomBg: "gameRoomBg",
 } as const;
 
 type Textures = (typeof Textures)[keyof typeof Textures];
