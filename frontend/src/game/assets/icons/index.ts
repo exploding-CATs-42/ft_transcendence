8a417fdd1c516c3fc91873f352fc0e78cb9eaaff
Andrii Kovtunets <akovtune@student.codam.nl>
2026-05-22 21:20:07 +0200

    feat(frontend): add fullscreen toggle icon to the game assets

---
+++ frontend/src/game/assets/icons/index.ts
@@ -0,0 +1,8 @@
+// svg used inside Phaser
+// unfortunately have to be placed inside public/ folder
+// and imported like below
+// it has something to do with how Vite processes svg files
+// when they are inside src/ folder
+// and files inside public/ folder are not processed by Vite
+// so it works just fine
+export const fullScreenToggle = "/game_assets/fullscreen-toggle.svg";
