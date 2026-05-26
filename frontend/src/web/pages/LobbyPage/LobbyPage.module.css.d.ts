5eeba32bf993e0e167618bfc4331b6eace1b74fd
osivkov <sivkovfollow@gmail.com>
2026-05-26 16:02:59 +0200

    fix(frontend): apply join modal review changes

--- frontend/src/web/pages/LobbyPage/LobbyPage.module.css.d.ts
+++ frontend/src/web/pages/LobbyPage/LobbyPage.module.css.d.ts
@@ -2,16 +2,18 @@ declare const styles: {
   readonly "button": string;
   readonly "buttons": string;
   readonly "color": string;
+  readonly "createLink": string;
+  readonly "createText": string;
+  readonly "icon": string;
+  readonly "joinButton": string;
+  readonly "joinModal": string;
+  readonly "joinModalContent": string;
   readonly "list": string;
   readonly "listSection": string;
-  readonly "pageContainer": string;
-  readonly "joinModal": string;
-  readonly "modalTitle": string;
-  readonly "modalInput": string;
-  readonly "joinButton": string;
-  readonly "createText": string;
-  readonly "createLink": string;
   readonly "matchButton": string;
+  readonly "modalInput": string;
+  readonly "modalTitle": string;
+  readonly "pageContainer": string;
 };
 export = styles;
 
