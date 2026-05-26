2d9320ba424e705f5582f574d345011c585406b9
rmzvr <rmzvr@proton.me>
2026-05-26 16:12:15 +0200

    feat(backend): add toWaitingPlayerView mapper function

---
+++ backend/src/game/mappers.ts
@@ -0,0 +1,7 @@
+import { WaitingPlayerView } from "../types";
+import { Player } from "./types";
+
+export const toWaitingPlayerView = (p: Player): WaitingPlayerView => ({
+  id: p.id,
+  name: p.name,
+});
