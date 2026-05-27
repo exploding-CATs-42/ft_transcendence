dfc41afcc51370537d4ed80d17f84aab09348741
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-05-27 15:30:43 +0200

    feat(frontend): switch default scene to GameRoom
    I switched default scene to be able to see changes that I make
    during development. It has to be updated back to WaitingRoom later

--- frontend/src/game/scenes/Preloader.ts
+++ frontend/src/game/scenes/Preloader.ts
@@ -40,7 +40,7 @@ export class Preloader extends Scene {
     //  For example, you can define global animations here, so we can use them in other scenes.
 
     //  Move to the Game scene. You could also swap this for a Scene Transition, such as a camera fade.
-    this.scene.start(Scenes.WaitingRoom);
+    this.scene.start(Scenes.GameRoom);
   }
 
   private addProgressBar() {
