26eb671841609d9bdf016ca19898510d09628329
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-05-27 15:28:10 +0200

    assets: add cards spritesheet file

--- frontend/src/game/constants/assets.ts
+++ frontend/src/game/constants/assets.ts
@@ -4,6 +4,7 @@ export const Textures = {
   waitingRoomBg: "waitingRoomBg",
   gameRoomBg: "gameRoomBg",
   fullScreenToggle: "fullscreenToggle",
+  cards: "cards",
 } as const;
 
 type Textures = (typeof Textures)[keyof typeof Textures];
