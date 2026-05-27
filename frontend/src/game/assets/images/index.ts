0b3fa8bd2b7fb23ddbc62c9227542aa6a0d9c559
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-05-27 15:28:10 +0200

    assets: add cards spritesheet file

--- frontend/src/game/assets/images/index.ts
+++ frontend/src/game/assets/images/index.ts
@@ -2,3 +2,4 @@ export { default as preloaderBg } from "./preloader_background.png";
 export { default as avatar } from "./avatar.png";
 export { default as waitingRoomBg } from "./waiting_room_background.png";
 export { default as gameRoomBg } from "./game_room_background.png";
+export { default as cards } from "./cards.webp";
