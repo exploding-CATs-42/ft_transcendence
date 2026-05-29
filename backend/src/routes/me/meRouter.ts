5597ab6b884891e65d053bafa8cabb2f2997da92
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-05-29 11:20:10 +0200

    refactor:(backend) consolidate middleware imports for cleaner code structure

--- backend/src/routes/me/meRouter.ts
+++ backend/src/routes/me/meRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { authMiddleware } from "../../middlewares/authMiddleware";
+import { authMiddleware } from "../../middlewares";
 import { updateMeController } from "../../controllers/meController";
 
 export const meRouter = express.Router();
