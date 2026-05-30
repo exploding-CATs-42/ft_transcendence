cc1e254bd23475441b32e60981a51e277bfadfaa
rmzvr <rmzvr@proton.me>
2026-05-30 12:57:20 +0200

    feat(backend): add COUNTDOWN_CANCELED type to GameEmitter

--- backend/src/game/emitters.ts
+++ backend/src/game/emitters.ts
@@ -1,11 +1,11 @@
 export const GameEmitterType = {
   COUNTDOWN_STARTED: "COUNTDOWN_STARTED",
+  COUNTDOWN_CANCELED: "COUNTDOWN_CANCELED",
 } as const;
 
 export type GameEmitterType =
   (typeof GameEmitterType)[keyof typeof GameEmitterType];
 
-export type GameEmitter = {
-  type: typeof GameEmitterType.COUNTDOWN_STARTED;
-  endsAt: number;
-};
+export type GameEmitter =
+  | { type: typeof GameEmitterType.COUNTDOWN_STARTED; endsAt: number }
+  | { type: typeof GameEmitterType.COUNTDOWN_CANCELED };
