8786bab484f9ebee5882fc006b78cc352ca6bdaf
rmzvr <rmzvr@proton.me>
2026-05-31 12:24:11 +0200

    refactor(backend): move game emitters to separate functions

--- backend/src/game/emitters.ts
+++ backend/src/game/emitters.ts
@@ -1,3 +1,5 @@
+import { START_GAME_COUNTDOWN_MS } from "../constants/game";
+
 export const GameEmitterType = {
   GAME_STARTED: "GAME_STARTED",
   COUNTDOWN_STARTED: "COUNTDOWN_STARTED",
@@ -11,3 +13,16 @@ export type GameEmitter =
   | { type: typeof GameEmitterType.GAME_STARTED }
   | { type: typeof GameEmitterType.COUNTDOWN_STARTED; endsAt: number }
   | { type: typeof GameEmitterType.COUNTDOWN_CANCELED };
+
+export const countdownStarted = (): GameEmitter => ({
+  type: GameEmitterType.COUNTDOWN_STARTED,
+  endsAt: Date.now() + START_GAME_COUNTDOWN_MS,
+});
+
+export const countdownCanceled = (): GameEmitter => ({
+  type: GameEmitterType.COUNTDOWN_CANCELED,
+});
+
+export const gameStarted = (): GameEmitter => ({
+  type: GameEmitterType.GAME_STARTED,
+});
