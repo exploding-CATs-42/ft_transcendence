1b214a552626e65622f699a54dbe34080c84ba32
rmzvr <rmzvr@proton.me>
2026-06-01 17:48:05 +0200

    refactor(frontend): add getAccessToken utility function for retrieving access token from local storage

---
+++ frontend/src/web/utils/getAccessToken.ts
@@ -0,0 +1,9 @@
+import { AUTH_STORAGE_KEY } from "constants";
+
+export const getAccessToken = (): string | null => {
+  const keys = localStorage.getItem(AUTH_STORAGE_KEY);
+
+  if (!keys) return null;
+
+  return JSON.parse(keys).accessToken ?? null;
+};
