0f3d727b85c20d1b9cda7bcb34de39be71ffcc71
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-02 16:38:46 +0200

    refactor(frontend): add ProfileUser interface to front

--- frontend/src/web/pages/ProfilePage/types/ProfileUser.ts
+++ frontend/src/web/pages/ProfilePage/types/ProfileUser.ts
@@ -1,7 +1,20 @@
-export type ProfileUser = {
+export interface ProfileUser {
   id: string;
   username: string;
-  avatarUrl: string;
   isOnline: boolean;
-  lastSeenAt: Date;
-};
+  avatarUrl: string | null;
+  lastSeenAt: Date | null;
+}
+
+export interface UserStats {
+  totalMatches: number;
+  wins: number;
+}
+
+export interface PublicProfileUser extends ProfileUser, UserStats {}
+
+export interface SelfProfileUser extends ProfileUser {
+  email: string;
+}
+
+export interface MeUser extends SelfProfileUser, UserStats {}
