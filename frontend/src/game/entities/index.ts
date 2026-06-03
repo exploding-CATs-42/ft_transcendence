e65f0bc542742d7bc064be366f5dba7266c4beae
Andrii Kovtunets <akovtune@student.codam.nl>
2026-06-03 18:44:50 +0200

    feat(frontend): add Button class

--- frontend/src/game/entities/index.ts
+++ frontend/src/game/entities/index.ts
@@ -3,3 +3,4 @@ export { GraphicPlayer } from "./GraphicPlayer";
 export { OpponentHand } from "./OpponentHand";
 export { Hand } from "./Hand";
 export { PlayerSeat } from "./PlayerSeat";
+export { Button } from "./Button";
