c2e6aba4a2997dafcf6c9c946d5f00465245fce3
akovtune <akovtune@student.codam.nl>
2026-06-03 16:44:43 +0200

    feat(frontend): add CardConfig type

--- frontend/src/game/@types/index.ts
+++ frontend/src/game/@types/index.ts
@@ -2,3 +2,4 @@ export type { Point } from "./Point";
 export type { LabelConfig } from "./LabelConfig";
 export type { SpacingConfig } from "./SpacingConfig";
 export type { Size } from "./Size";
+export type { CardConfig } from "./CardConfig";
