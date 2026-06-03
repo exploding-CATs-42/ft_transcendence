0da7790c7dc88ba4fb81d360244092b24c327823
rmzvr <rmzvr@proton.me>
2026-06-03 12:09:32 +0200

    refactor(backend): add barrel file for game/types

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,6 +1,4 @@
 export * from "./actions";
-export * from "../game/types/card";
 export * from "./events";
-export * from "../game/types/game";
-export * from "../game/types/turn";
+export * from "../game/types";
 export * from "./users";
