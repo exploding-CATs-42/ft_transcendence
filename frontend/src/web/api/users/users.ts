9e8ecc6b198cd10fc402d328f161ffb40a31a195
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-05 14:24:07 +0200

    feat(frontend): rename getLastMatches to getUserGames for consistency in API usage

--- frontend/src/web/api/users/users.ts
+++ frontend/src/web/api/users/users.ts
@@ -2,13 +2,11 @@ import { api } from "../axios";
 import type { UserGameHistoryItem } from "components/MatchListItem/types";
 import type { UserId } from "pages/ProfilePage/types/ProfileUser";
 
-const getLastMatches = async (
-  userId: UserId,
-): Promise<UserGameHistoryItem[]> => {
+const getUserGames = async (userId: UserId): Promise<UserGameHistoryItem[]> => {
   const result = await api.get(`users/${userId}/games`);
   return result.data.games;
 };
 
 export default {
-  getLastMatches,
+  getUserGames,
 };
