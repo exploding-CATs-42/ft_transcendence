a1415af1684f568941410f383b767f196baefcfe
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-06 15:22:05 +0200

    feat(frontend): add input fields and styles ro EditPlayerModal

--- frontend/src/web/pages/ProfilePage/components/EditPlayerModal/EditPlayerModal.module.css.d.ts
+++ frontend/src/web/pages/ProfilePage/components/EditPlayerModal/EditPlayerModal.module.css.d.ts
@@ -1,6 +1,9 @@
 declare const styles: {
+  readonly "avatar": string;
   readonly "editPlayerButton": string;
+  readonly "editPlayerForm": string;
   readonly "editPlayerModal": string;
+  readonly "modalTitle": string;
 };
 export = styles;
 
