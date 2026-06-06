c052db4c400d8a92033630037f6f5f2c90b00d07
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-06 21:37:36 +0200

    feat(frontend): add name label

--- frontend/src/web/pages/AboutPage/components/DevCard/DevCard.tsx
+++ frontend/src/web/pages/AboutPage/components/DevCard/DevCard.tsx
@@ -9,6 +9,7 @@ const ContactCard = (props: DevProfile) => {
   return (
     <div className={s.card}>
       <img className={s.photo} src={photoUrl} alt={name} width={464} />
+      <h3 className={s.name}>{name}</h3>
     </div>
   );
 };
