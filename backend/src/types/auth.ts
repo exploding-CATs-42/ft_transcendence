5694a8161f0e97da0b3505fd49eaaa801749c327
rmzvr <rmzvr@proton.me>
2026-06-06 13:07:11 +0200

    refactor(backend): move UserId type to users.ts

--- backend/src/types/auth.ts
+++ backend/src/types/auth.ts
@@ -1,8 +1,6 @@
 import { Request } from "express";
 import { PublicUser, User } from "./users";
 
-export type UserId = string;
-
 export interface AuthResponse {
   user: User;
   accessToken: string;
