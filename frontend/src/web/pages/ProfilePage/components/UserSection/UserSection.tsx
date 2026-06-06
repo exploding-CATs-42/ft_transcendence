6ccbcdb3785be0e0959286eda320cf4b82d37f5a
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-06 15:51:09 +0200

    feat(frontend): update ProfilePage and UserSection to support MyProfileUser type and conditional rendering

--- frontend/src/web/pages/ProfilePage/components/UserSection/UserSection.tsx
+++ frontend/src/web/pages/ProfilePage/components/UserSection/UserSection.tsx
@@ -4,14 +4,20 @@ import { useAuth, useModal } from "hooks";
 import api from "api";
 // Local level
 import { EditPlayerModal } from "../../components";
-import type { ProfileUser } from "../../types";
+import type { MyProfileUser, ProfileUser } from "../../types";
 import s from "./UserSection.module.css";
 
-interface Props {
-  user: ProfileUser;
-}
+type Props =
+  | {
+      isMyProfile: true;
+      user: MyProfileUser;
+    }
+  | {
+      isMyProfile: false;
+      user: ProfileUser;
+    };
 
-const UserSection = ({ user }: Props) => {
+const UserSection = ({ user, isMyProfile }: Props) => {
   const { clearAccessToken } = useAuth();
   const [isOpenEditPlayerModal, toggleOpenEditPlayerModal] = useModal();
 
@@ -31,26 +37,33 @@ const UserSection = ({ user }: Props) => {
     <Section className={s.section}>
       <Avatar className={s.avatar} variant="profile" src={user.avatarUrl} />
       <span className={s.name}>{user.username}</span>
-      <Button
-        className={s.editButton}
-        onClick={() => {
-          toggleOpenEditPlayerModal(true);
-        }}
-      >
-        <Icon name="pencil" className={s.editIcon} width={15} height={15} />
-      </Button>
+
+      {isMyProfile && (
+        <Button
+          className={s.editButton}
+          onClick={() => {
+            toggleOpenEditPlayerModal(true);
+          }}
+        >
+          <Icon name="pencil" className={s.editIcon} width={15} height={15} />
+        </Button>
+      )}
+
       <Button className={s.logoutButton} onClick={logoutUser}>
         <Icon name="log-out" className={s.logoutIcon} width={15} height={15} />
         <span className={s.logoutText}>Sign out</span>
       </Button>
 
-      <EditPlayerModal
-        isOpen={isOpenEditPlayerModal}
-        toggleModal={() => toggleOpenEditPlayerModal(false)}
-        onSubmit={() => {
-          onSubmit();
-        }}
-      />
+      {isMyProfile && (
+        <EditPlayerModal
+          isOpen={isOpenEditPlayerModal}
+          toggleModal={() => toggleOpenEditPlayerModal(false)}
+          onSubmit={() => {
+            onSubmit();
+          }}
+          user={user}
+        />
+      )}
     </Section>
   );
 };
