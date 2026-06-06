2d031070e09f4cb293170d9fce9e08679131d1fd
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-06 15:23:24 +0200

    feat(api): add getUserById function to retrieve user details by ID

--- frontend/src/web/api/users/users.ts
+++ frontend/src/web/api/users/users.ts
@@ -1,12 +1,18 @@
 import { api } from "../axios";
 import type { UserGameHistoryItem } from "components/MatchListItem/types";
-import type { UserId } from "pages/ProfilePage/types/ProfileUser";
+import type { ProfileUser, UserId } from "pages/ProfilePage/types/ProfileUser";
 
 const getUserGames = async (userId: UserId): Promise<UserGameHistoryItem[]> => {
   const result = await api.get(`/users/${userId}/games`);
   return result.data.games;
 };
 
+const getUserById = async (userId: UserId): Promise<ProfileUser> => {
+  const result = await api.get(`users/${userId}`);
+  return result.data.user;
+};
+
 export default {
   getUserGames,
+  getUserById,
 };
