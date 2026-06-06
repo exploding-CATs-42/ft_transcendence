ecdce7b0938e1220cfcc3e269bcba1b787962eaa
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-06 21:28:15 +0200

    feat(frontend): add photo to the DevCard

--- frontend/src/web/pages/AboutPage/components/DevCard/DevCard.tsx
+++ frontend/src/web/pages/AboutPage/components/DevCard/DevCard.tsx
@@ -1,8 +1,16 @@
+// Project level
+import type { DevProfile } from "types";
 // CSS
 import s from "./DevCard.module.css";
 
-const ContactCard = () => {
-  return <div className={s.card}></div>;
+const ContactCard = (props: DevProfile) => {
+  const { name, photoUrl } = props;
+
+  return (
+    <div className={s.card}>
+      <img className={s.photo} src={photoUrl} alt={name} width={464} />
+    </div>
+  );
 };
 
 export default ContactCard;
