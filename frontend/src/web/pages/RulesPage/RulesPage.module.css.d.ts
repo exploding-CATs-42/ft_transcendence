b942877c500cdb1a8250d1d242374e954dae3e84
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-06 14:53:11 +0200

    feat(frontend): add rulebook PDF download link

--- frontend/src/web/pages/RulesPage/RulesPage.module.css.d.ts
+++ frontend/src/web/pages/RulesPage/RulesPage.module.css.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "bottomText": string;
+  readonly "downloadLink": string;
   readonly "pageContainer": string;
   readonly "topText": string;
   readonly "video": string;
