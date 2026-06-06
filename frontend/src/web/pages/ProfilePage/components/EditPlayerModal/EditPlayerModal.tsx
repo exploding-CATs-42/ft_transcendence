cce56b7d41e9fa0d19d84f1381ee0cd3547b91e9
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-06 15:22:05 +0200

    feat(frontend): add input fields and styles ro EditPlayerModal

--- frontend/src/web/pages/ProfilePage/components/EditPlayerModal/EditPlayerModal.tsx
+++ frontend/src/web/pages/ProfilePage/components/EditPlayerModal/EditPlayerModal.tsx
@@ -1,20 +1,49 @@
-import { Button, Modal } from "components";
+import {
+  Avatar,
+  Button,
+  EmailInput,
+  FormField,
+  Modal,
+  NameInput,
+  PasswordInput,
+} from "components";
 import s from "./EditPlayerModal.module.css";
+import type { MyProfileUser } from "pages/ProfilePage/types";
 
 interface Props {
   isOpen: boolean;
   toggleModal: () => void;
   onSubmit: () => void;
+  user: MyProfileUser;
 }
 
-const EditPlayerModal = ({ isOpen, toggleModal, onSubmit }: Props) => {
+const EditPlayerModal = ({ isOpen, toggleModal, onSubmit, user }: Props) => {
   return (
     <Modal
       className={s.editPlayerModal}
       isOpen={isOpen}
       toggleModal={toggleModal}
     >
-      <form>
+      <form className={s.editPlayerForm}>
+        <h2 className={s.modalTitle}> Profile Settings </h2>
+        <Avatar className={s.avatar} variant="profile" src={user.avatarUrl} />
+
+        <FormField>
+          <EmailInput />
+        </FormField>
+
+        <FormField>
+          <NameInput />
+        </FormField>
+
+        <FormField>
+          <PasswordInput />
+        </FormField>
+
+        <FormField>
+          <PasswordInput />
+        </FormField>
+
         <Button
           className={s.editPlayerButton}
           onSubmit={() => {
