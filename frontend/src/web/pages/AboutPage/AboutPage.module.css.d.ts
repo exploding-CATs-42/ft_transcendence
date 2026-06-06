ed8c77a00195fd7fb863ef9b5f6eae89c89b11a9
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-06 21:13:19 +0200

    feat(frontend): add first section title

--- frontend/src/web/pages/AboutPage/AboutPage.module.css.d.ts
+++ frontend/src/web/pages/AboutPage/AboutPage.module.css.d.ts
@@ -1,6 +1,7 @@
 declare const styles: {
   readonly "pageContainer": string;
   readonly "section": string;
+  readonly "sectionHeader": string;
 };
 export = styles;
 
--- frontend/src/web/pages/AboutPage/AboutPage.tsx
+++ frontend/src/web/pages/AboutPage/AboutPage.tsx
@@ -4,7 +4,9 @@ import s from "./AboutPage.module.css";
 const AboutPage = () => {
   return (
     <div className={s.pageContainer}>
-      <section className={s.section}></section>
+      <section className={s.section}>
+        <h2 className={s.sectionHeader}>About the project</h2>
+      </section>
     </div>
   );
 };
