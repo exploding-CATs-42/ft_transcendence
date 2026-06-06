7452375bb746a32113a824d6e8a38410b18e5231
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-06 15:11:48 +0200

    feat(frontend): add play button on rules page

--- frontend/src/web/pages/RulesPage/RulesPage.module.css.d.ts
+++ frontend/src/web/pages/RulesPage/RulesPage.module.css.d.ts
@@ -2,7 +2,9 @@ declare const styles: {
   readonly "bottomText": string;
   readonly "downloadLink": string;
   readonly "pageContainer": string;
+  readonly "playButton": string;
   readonly "poster": string;
+  readonly "posterContainer": string;
   readonly "topText": string;
   readonly "video": string;
   readonly "videoWrapper": string;
