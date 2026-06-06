0faf7d5753b81aa7fda2a8e66b47a4db64055e0b
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-06 21:15:52 +0200

    feat(frontend): add project description paragraph

--- frontend/src/web/pages/AboutPage/AboutPage.tsx
+++ frontend/src/web/pages/AboutPage/AboutPage.tsx
@@ -6,6 +6,16 @@ const AboutPage = () => {
     <div className={s.pageContainer}>
       <section className={s.section}>
         <h2 className={s.sectionHeader}>About the project</h2>
+        <p className={s.description}>
+          Exploding CATs (also known as ft_transcendence) is the final project
+          of the 42 curriculum. This project gives students the ability to
+          choose what they want to build and is designed to challenge all the
+          skills acquired throughout the program. We went with a web based "
+          Exploding kittens " game clone. It was fun to plan and build it, while
+          applying principles of collaboration, problem-solving, project
+          management, and software development that we learned through the
+          intensive, peer-driven learning experience that is 42.
+        </p>
       </section>
     </div>
   );
