7af56e2c111dae1ba7a25f13b59fe0cc5dbc854f
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-07 16:06:41 +0200

    refactor(frontend): replace LoginPage and RegisterPage with AuthPage
    I extracted unique code:
    - from RegisterPage into RegisterForm
    - from LoginPage into LoginForm
    
    and same code into AuthPage, that decides which of the forms to display,
    depending on the url thanks to useLocation hook from react-router-dom.
    I did this to avoid css styles duplication between the 2 pages,
    and therefore unifying their styling and simplifying any possible future updates

---
+++ frontend/src/web/pages/AuthPage/AuthPage.module.css.d.ts
@@ -0,0 +1,10 @@
+declare const styles: {
+  readonly "backgroundContainer": string;
+  readonly "blur": string;
+  readonly "formBackground": string;
+  readonly "kittensSpan": string;
+  readonly "pageContainer": string;
+  readonly "title": string;
+};
+export = styles;
+
