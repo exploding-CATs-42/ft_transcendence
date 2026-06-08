3a5055c23a7c57a1439b559e5d13012098ebd01a
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-08 15:46:19 +0200

    style(frontend): update EditPlayerModal styles and add new button styles

--- frontend/src/web/pages/ProfilePage/components/EditPlayerModal/EditPlayerModal.module.css.d.ts
+++ frontend/src/web/pages/ProfilePage/components/EditPlayerModal/EditPlayerModal.module.css.d.ts
@@ -1,9 +1,12 @@
 declare const styles: {
   readonly "avatar": string;
+  readonly "changeFormButton": string;
   readonly "editPlayerButton": string;
   readonly "editPlayerForm": string;
   readonly "editPlayerModal": string;
+  readonly "inputContainer": string;
   readonly "modalTitle": string;
+  readonly "underline": string;
 };
 export = styles;
 
