56bc6164aa6dc22ef650eba2c5c3bab4afcd9827
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-09 20:55:36 +0200

    refactor(frontend): split friends API into its own module and update references

--- frontend/src/web/api/index.ts
+++ frontend/src/web/api/index.ts
@@ -1,10 +1,11 @@
 import auth from "./auth/auth";
+import friends from "./friends/friends";
 import games from "./games/games";
 import me from "./me/me";
 import users from "./users/users";
 //import your api endpoint group here
 
-export default { auth, games, me, users }; //end then export it within this default object
+export default { auth, games, me, users, friends }; //end then export it within this default object
 
 //this way you will be able to use api in your code like :
 //import api from "api"
