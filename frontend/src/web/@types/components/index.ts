e30b52832060b6354ef74348407634fd2e5fc066
Andrii Kovtunets <akovtune@student.codam.nl>
2026-06-09 15:00:47 +0200

    chore(frontend): reexport IconName type from "types" folder for prettier imports

--- frontend/src/web/@types/components/index.ts
+++ frontend/src/web/@types/components/index.ts
@@ -1,3 +1,4 @@
 export type { InputType, InputStatus } from "components/Input/types";
 export type { PhaserGameRef } from "components/PhaserGame/types";
 export type { LobbyMatch, MatchSlot } from "components/MatchListItem/types";
+export type { IconName } from "components/Icon/Icon";
