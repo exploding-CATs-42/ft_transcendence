917b5e7c62caa31c01b60bc1932378c6fa308872
rmzvr <rmzvr@proton.me>
2026-06-12 16:43:24 +0200

    feat(frontend): implement authentication-based socket connection management

--- frontend/src/web/context/SocketProvider.tsx
+++ frontend/src/web/context/SocketProvider.tsx
@@ -1,7 +1,8 @@
 // Libraries
-import { type ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
 // Project level
 import { socket } from "socket";
+import { useAuth } from "hooks";
 // Local level
 import { SocketContext } from "./SocketContext";
 
@@ -10,6 +11,20 @@ interface Props {
 }
 
 const SocketProvider = ({ children }: Props) => {
+  const { authStatus, accessToken } = useAuth();
+
+  useEffect(() => {
+    if (authStatus === "authenticated") {
+      socket.auth = {
+        token: accessToken,
+      };
+
+      socket.connect();
+    } else {
+      socket.disconnect();
+    }
+  }, [accessToken, authStatus]);
+
   return (
     <SocketContext.Provider value={{ socket }}>
       {children}
