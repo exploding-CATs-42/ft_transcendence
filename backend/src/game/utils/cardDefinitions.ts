74472d76d7af4109435a9e8e03cde5082ece9a38
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-12 21:50:01 +0200

    refactor(backend): convert cardDefinitions.ts into an actual utility function

--- backend/src/game/utils/cardDefinitions.ts
+++ backend/src/game/utils/cardDefinitions.ts
@@ -3,7 +3,13 @@ import { z } from "zod";
 // Project level
 import rawCards from "../../constants/cards.json";
 // Local level
-import { CardDefinitionSchema } from "../types/cards/cardDefinition";
+import {
+  CardDefinitionSchema,
+  CardDefinition,
+} from "../types/cards/cardDefinition";
 
 const CardsSchema = z.array(CardDefinitionSchema);
-export const cardDefinitions = CardsSchema.parse(rawCards);
+
+export const getParsedCardDefinitions = (): CardDefinition[] => {
+  return CardsSchema.parse(rawCards);
+};
