513162092aa57ebf316e92acb3bd3b3da0f6a0da
rmzvr <rmzvr@proton.me>
2026-06-12 16:42:12 +0200

    feat(frontend): wrap App component with SocketProvider

--- frontend/src/web/main.tsx
+++ frontend/src/web/main.tsx
@@ -2,14 +2,16 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 // Project level
-import { AuthProvider } from "context";
+import { AuthProvider, SocketProvider } from "context";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SocketProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SocketProvider>
   </AuthProvider>,
 );
