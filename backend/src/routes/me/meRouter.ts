8479a9cc70ba885b33506b09ad18184d467922bd
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-13 19:59:51 +0200

    feat(backend): add avatar upload route to meRouter

--- backend/src/routes/me/meRouter.ts
+++ backend/src/routes/me/meRouter.ts
@@ -1,9 +1,20 @@
 // Project level
-import { getMeController, getMeGames, updateMeController } from "controllers";
+import {
+  getMeController,
+  getMeGames,
+  updateMeAvatarController,
+  updateMeController,
+} from "controllers";
 import { createAuthenticatedRouter, errorHandler } from "utils";
+import photoUpload from "../../middlewares/cloudinary/upload";
 
 export const meRouter = createAuthenticatedRouter();
 
 meRouter.patch("/", errorHandler(updateMeController));
+meRouter.patch(
+  "/avatar",
+  photoUpload.single("avatar"),
+  errorHandler(updateMeAvatarController),
+);
 meRouter.get("/", errorHandler(getMeController));
 meRouter.get("/games", errorHandler(getMeGames));
