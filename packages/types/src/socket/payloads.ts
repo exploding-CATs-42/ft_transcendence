5f81bcd76b5ddd27f28b8788016dd92a9efc38fa
rmzvr <rmzvr@proton.me>
2026-06-13 20:51:16 +0200

    feat: add PlayerJoinedPayload to socket types

--- packages/types/src/socket/payloads.ts
+++ packages/types/src/socket/payloads.ts
@@ -1,5 +1,11 @@
+import type { WaitingPlayerView } from "./views";
+
 export interface PlayerIdPayload {
   playerId: string;
+}
+
+export interface PlayerJoinedPayload {
+  player: WaitingPlayerView;
 }
 
 export interface CountdownStartedPayload {
