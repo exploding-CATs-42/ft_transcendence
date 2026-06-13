70e009360c3c94644c1bb6f82c2dc17b8b6d4a6d
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-13 14:11:52 +0200

    feat(backend): implement photo upload middleware with MIME type validation

--- backend/src/middlewares/cloudinary/upload.ts
+++ backend/src/middlewares/cloudinary/upload.ts
@@ -1,5 +1,5 @@
 import path from "node:path";
-import multer from "multer";
+import multer, { FileFilterCallback } from "multer";
 import { Request } from "express";
 
 const photoStorage = multer.diskStorage({
@@ -20,3 +20,22 @@ const photoStorage = multer.diskStorage({
     cb(null, filename);
   },
 });
+
+export const photoUpload = multer({
+  storage: photoStorage,
+
+  fileFilter: (
+    _req: Request,
+    file: Express.Multer.File,
+    cb: FileFilterCallback,
+  ) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+      return;
+    }
+
+    cb(new Error("Unsupported file format"));
+  },
+});
+
+export default photoUpload;
