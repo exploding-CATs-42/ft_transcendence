e70281f05877a20579b771a836035c76e41655e7
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-13 21:30:43 +0200

    feat(frontend): add spinner GIF asset to image exports

--- frontend/src/web/assets/images/index.ts
+++ frontend/src/web/assets/images/index.ts
@@ -2,3 +2,4 @@ export { default as CatomicBomb } from "./catomic-bomb/catomic-bomb.png";
 export { default as FireCat } from "./fire-cat/fire-cat.png";
 export { default as Avatar } from "./avatar/avatar-193w.png";
 export { default as Loader } from "./loader/loader.gif";
+export { default as Spinner } from "./spinner/spinner.gif";
