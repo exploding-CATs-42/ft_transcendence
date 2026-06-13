fe08f6fd13df65a73e6565a5b204b94bef4579b1
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-13 14:48:17 +0200

    feat(frontend): implement AvatarWithAdd component with styling

--- frontend/src/web/components/AvatarWithAdd/AvatarWithAdd.tsx
+++ frontend/src/web/components/AvatarWithAdd/AvatarWithAdd.tsx
@@ -1,5 +1,26 @@
-export function AvatarWithAdd() {
-  return <></>;
+import s from "./AvatarWithAdd.module.css";
+import Avatar from "components/Avatar/Avatar";
+
+type Props = {
+  src: string | null;
+  onClick: () => void;
+};
+
+export function AvatarWithAdd({ src, onClick }: Props) {
+  return (
+    <div className={s.wrapper}>
+      <Avatar className={s.avatar} variant="profile" src={src} />
+
+      <button
+        type="button"
+        className={s.plusButton}
+        onClick={onClick}
+        aria-label="Change avatar"
+      >
+        +
+      </button>
+    </div>
+  );
 }
 
 export default AvatarWithAdd;
