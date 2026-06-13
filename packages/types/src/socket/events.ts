b09a5e6fa40e114f2578ee09d9a2f53a8f96e176
rmzvr <rmzvr@proton.me>
2026-06-13 20:01:48 +0200

    refactor: rename ClientEventType to ClientEvents and update references

--- packages/types/src/socket/events.ts
+++ packages/types/src/socket/events.ts
@@ -1,12 +1,12 @@
-export const ClientEventType = {
+export const ClientEvents = {
   JOIN_GAME: "join-game",
   LEAVE_GAME: "leave-game",
   CONFIRM_START: "confirm-start",
   CANCEL_START: "cancel-start",
 } as const;
 
-export type ClientEventType =
-  (typeof ClientEventType)[keyof typeof ClientEventType];
+export type ClientEvents =
+  (typeof ClientEvents)[keyof typeof ClientEvents];
 
 export const PublicEventType = {
   PLAYER_JOINED: "player-joined",
