7b23aa287d76864c93831ec00fafeb4c78ab58ba
rmzvr <rmzvr@proton.me>
2026-06-13 19:50:39 +0200

    refactor(backend): move ErrorEventType to shared types and update imports

--- packages/types/src/socket/events.ts
+++ packages/types/src/socket/events.ts
@@ -7,3 +7,13 @@ export const ClientEventType = {
 
 export type ClientEventType =
   (typeof ClientEventType)[keyof typeof ClientEventType];
+
+export const ErrorEventType = {
+  JOIN_GAME_ERROR: "join-game-error",
+  LEAVE_GAME_ERROR: "leave-game-error",
+  CONFIRM_START_ERROR: "confirm-start-error",
+  CANCEL_START_ERROR: "cancel-start-error",
+} as const;
+
+export type ErrorEventType =
+  (typeof ErrorEventType)[keyof typeof ErrorEventType];
