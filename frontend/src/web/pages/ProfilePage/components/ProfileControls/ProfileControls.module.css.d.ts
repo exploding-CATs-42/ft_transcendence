ef5c0aa040dcd7a0d0575e4ba5f2995776a60c49
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-14 11:41:40 +0200

    feat(frontend): move edit and logout functionality into ProfileControls component

---
+++ frontend/src/web/pages/ProfilePage/components/ProfileControls/ProfileControls.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "editButton": string;
+  readonly "editIcon": string;
+  readonly "logoutButton": string;
+  readonly "logoutIcon": string;
+  readonly "logoutText": string;
+};
+export = styles;
+
