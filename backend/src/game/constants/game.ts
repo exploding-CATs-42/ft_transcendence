7662c4f4bdb520ea0de3e7fe4fa21ac8fdcf43dc
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-14 16:25:34 +0200

    refactor(backend): remove unused game constants

--- backend/src/game/constants/game.ts
+++ backend/src/game/constants/game.ts
@@ -1,5 +1,5 @@
 // Local level
-import { Player, GameId, GameRules, GameState, TurnState } from "game/types";
+import { GameRules } from "game/types";
 
 export const DEFAULT_GAME_RULES: GameRules = {
   dealtCardsPerPlayer: 7,
@@ -11,29 +11,6 @@ export const DEFAULT_GAME_RULES: GameRules = {
   maxPlayers: 5,
   fasterVariantRemoveFraction: 0.3,
   nopeWindowMs: 5000,
-};
-
-export const DEFAULT_PLAYER: Player = {
-  id: "",
-  name: "",
-  hand: [],
-  isConfirmed: false,
-  isAlive: true,
-  turnOrder: 0,
-};
-
-export const DEFAULT_GAME_STATE: GameState = {
-  gameId: "" as GameId,
-  name: "",
-  maxPlayers: DEFAULT_GAME_RULES.maxPlayers,
-  players: [],
-  deck: [],
-  turn: {} as TurnState,
-  winnerId: null,
-  rules: DEFAULT_GAME_RULES,
-  createdAt: Date.now(),
-  startedAt: null,
-  finishedAt: null,
 };
 
 export const MIN_PLAYERS = 2;
