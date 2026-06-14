7bdfcaab6777caf09b48b50a1557a29d5a78a9a7
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-14 15:51:51 +0200

    test(backend): add transition test for waiting.starting state
    Simulates players joining the game and sending JOIN_GAME and
    CONFIRM_START events, and asserts that the game machine transitions
    to the waiting.starting state when all players are ready.

--- backend/tests/game/gameMachine.test.ts
+++ backend/tests/game/gameMachine.test.ts
@@ -2,7 +2,41 @@
 import { createActor } from "xstate";
 import { describe, it, expect } from "vitest";
 // Project level
-import { gameMachine, GameStates } from "game";
+import { gameMachine, GameStates, GameEvents } from "game";
+import { Player } from "game/types";
+
+const PLAYERS: Player[] = [
+  {
+    name: "player 1",
+    id: "1",
+    hand: [],
+    isAlive: true,
+    isConfirmed: false,
+    turnOrder: 0,
+  },
+  {
+    name: "player 2",
+    id: "2",
+    hand: [],
+    isAlive: true,
+    isConfirmed: false,
+    turnOrder: 1,
+  },
+];
+
+type Actor = ReturnType<typeof createActor<typeof gameMachine>>;
+
+const addPlayers = (actor: Actor, players: Player[]) => {
+  players.forEach((player) => {
+    actor.send({ type: GameEvents.JOIN_GAME, player });
+  });
+};
+
+const markAsReady = (actor: Actor, players: Player[]) => {
+  players.forEach((player) => {
+    actor.send({ type: GameEvents.CONFIRM_START, playerId: player.id });
+  });
+};
 
 describe("game machine", () => {
   it("starts in waiting.confirming state", () => {
@@ -12,6 +46,20 @@ describe("game machine", () => {
 
     expect(actor.getSnapshot().value).toEqual({
       [GameStates.WAITING]: GameStates.WAITING_CONFIRMING,
+    });
+  });
+
+  it("transitions to waiting.starting state when enough players confirmed their readiness", () => {
+    const actor = createActor(gameMachine);
+
+    console.log(PLAYERS);
+
+    actor.start();
+    addPlayers(actor, PLAYERS);
+    markAsReady(actor, PLAYERS);
+
+    expect(actor.getSnapshot().value).toEqual({
+      [GameStates.WAITING]: GameStates.WAITING_STARTING,
     });
   });
 });
