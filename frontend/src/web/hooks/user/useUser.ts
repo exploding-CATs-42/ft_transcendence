b5d7cf5efa5ee02dc87fb349415e9ac942b62c8f
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-14 23:17:49 +0200

    refactor(frontend): move getUser function to useUser hook

--- frontend/src/web/hooks/user/useUser.ts
+++ frontend/src/web/hooks/user/useUser.ts
@@ -1,8 +1,45 @@
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+import api from "api";
+import { getErrorMessage } from "utils";
+
 import type { MyProfileUser, ProfileUser } from "pages/ProfilePage/types";
-import { useState } from "react";
 
-export const useFriends = () => {
-  const [user, _setUser] = useState<ProfileUser | MyProfileUser | null>(null);
+interface Props {
+  userId: string | undefined;
+  isMyProfile: boolean;
+}
 
-  return user;
+export const useUser = ({ userId, isMyProfile }: Props) => {
+  const [user, setUser] = useState<ProfileUser | MyProfileUser | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        let data: ProfileUser | MyProfileUser | null = null;
+
+        if (isMyProfile) {
+          data = await api.me.getMe();
+        } else if (userId) {
+          data = await api.users.getUserById(userId);
+        }
+
+        setUser(data);
+      } catch (error) {
+        const errorMessage = getErrorMessage(error);
+        setUser(null);
+        toast(errorMessage);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, [isMyProfile, userId]);
+
+  return {
+    user,
+    userLoading: loading,
+  };
 };
