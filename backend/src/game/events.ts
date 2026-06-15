4569cf1c0f6f9420dbf8e0f4b13bc2fda96c8153
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-15 15:23:59 +0200

    refactor(backend): rename GameEmitters to GameOutEvents
    Moves definitions of the events emitted by the machine
    from `emitters.ts` to `events.ts`
    with renaming from `GameEmitters` to `GameOutEvents`

--- backend/src/game/events.ts
+++ backend/src/game/events.ts
@@ -1,5 +1,6 @@
 import { Player } from "game/types";
 
+// Events sent TO the machine
 export const GameEvents = {
   JOIN_GAME: "JOIN_GAME",
   LEAVE_GAME: "LEAVE_GAME",
@@ -14,3 +15,17 @@ export type GameEvent =
   | { type: typeof GameEvents.LEAVE_GAME; playerId: Player["id"] }
   | { type: typeof GameEvents.CONFIRM_START; playerId: Player["id"] }
   | { type: typeof GameEvents.CANCEL_START; playerId: Player["id"] };
+
+// Events emitted FROM the machine
+export const GameOutEvents = {
+  GAME_STARTED: "GAME_STARTED",
+  COUNTDOWN_STARTED: "COUNTDOWN_STARTED",
+  COUNTDOWN_CANCELED: "COUNTDOWN_CANCELED",
+} as const;
+
+export type GameOutEvents = (typeof GameOutEvents)[keyof typeof GameOutEvents];
+
+export type GameOutEvent =
+  | { type: typeof GameOutEvents.GAME_STARTED }
+  | { type: typeof GameOutEvents.COUNTDOWN_STARTED; endsAt: number }
+  | { type: typeof GameOutEvents.COUNTDOWN_CANCELED };
