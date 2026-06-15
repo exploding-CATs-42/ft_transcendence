47c895b5cfbed2d5f562064fd8af07f75349cc83
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-15 15:23:59 +0200

    refactor(backend): rename GameEmitters to GameOutEvents
    Moves definitions of the events emitted by the machine
    from `emitters.ts` to `events.ts`
    with renaming from `GameEmitters` to `GameOutEvents`

--- backend/src/game/emitters.ts
+++ backend/src/game/emitters.ts
@@ -1,28 +1,16 @@
 // Project level
 import { START_GAME_COUNTDOWN_MS } from "game/constants";
+import { GameOutEvent, GameOutEvents } from "game/events";
 
-export const GameEmitters = {
-  GAME_STARTED: "GAME_STARTED",
-  COUNTDOWN_STARTED: "COUNTDOWN_STARTED",
-  COUNTDOWN_CANCELED: "COUNTDOWN_CANCELED",
-} as const;
-
-export type GameEmitters = (typeof GameEmitters)[keyof typeof GameEmitters];
-
-export type GameEmitter =
-  | { type: typeof GameEmitters.GAME_STARTED }
-  | { type: typeof GameEmitters.COUNTDOWN_STARTED; endsAt: number }
-  | { type: typeof GameEmitters.COUNTDOWN_CANCELED };
-
-export const countdownStarted = (): GameEmitter => ({
-  type: GameEmitters.COUNTDOWN_STARTED,
+export const countdownStarted = (): GameOutEvent => ({
+  type: GameOutEvents.COUNTDOWN_STARTED,
   endsAt: Date.now() + START_GAME_COUNTDOWN_MS,
 });
 
-export const countdownCanceled = (): GameEmitter => ({
-  type: GameEmitters.COUNTDOWN_CANCELED,
+export const countdownCanceled = (): GameOutEvent => ({
+  type: GameOutEvents.COUNTDOWN_CANCELED,
 });
 
-export const gameStarted = (): GameEmitter => ({
-  type: GameEmitters.GAME_STARTED,
+export const gameStarted = (): GameOutEvent => ({
+  type: GameOutEvents.GAME_STARTED,
 });
