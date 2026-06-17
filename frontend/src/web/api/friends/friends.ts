61e2f2f8fdf034d0ebebc928ae16ac229ef6f97c
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-17 15:28:03 +0200

    refactor(frontend): update getUserFriends to use params and query from zod schemas

--- frontend/src/web/api/friends/friends.ts
+++ frontend/src/web/api/friends/friends.ts
@@ -1,13 +1,20 @@
+import type { ListFriendsQuery, UserIdParams } from "@exploding-cats/contracts";
 import { api } from "../axios";
-import type { FriendItem, UserId } from "pages/ProfilePage/types";
+import type { FriendItem } from "pages/ProfilePage/types";
 
 const getMeFriends = async (): Promise<FriendItem[]> => {
   const result = await api.get("/me/friends");
   return result.data.friends;
 };
 
-const getUserFriends = async (userId: UserId): Promise<FriendItem[]> => {
-  const result = await api.get(`/users/${userId}/friends`);
+const getUserFriends = async (
+  params: UserIdParams,
+  query?: ListFriendsQuery,
+): Promise<FriendItem[]> => {
+  const result = await api.get(`/users/${params.userId}/friends`, {
+    params: query,
+  });
+
   return result.data.friends;
 };
 
