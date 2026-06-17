a93789fc1f611705040ed2a4f4a111fe1e3bead6
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-17 20:03:39 +0200

    docs(backend): add explanation and examples for xstate emitters

--- backend/src/game/emitters.ts
+++ backend/src/game/emitters.ts
@@ -14,3 +14,38 @@ export const countdownCanceled = (): GameOutEvent => ({
 export const gameStarted = (): GameOutEvent => ({
   type: GameOutEvents.GAME_STARTED,
 });
+
+/* emitter - is a function that emits an "event" object to the "outside world",
+ * giving it it's type and optional payload.
+ * it takes as a parameter an object, containing machine context,
+ * and an event that triggered the emission
+ */
+
+// emitter example
+/*
+ import { GameContext } from "./gameMachine";
+ import { GameEvent } from "./events"
+ type GameEmitterArgs = {
+   context: GameContext;
+   event: GameEvent;
+ }
+ export const emitter = ({ context, event }: GameEmitterArgs): GameOutEvent => {
+   if (context.players.length < 1) console.log("it's lonely out here")
+   return { type: GameOutEvents.PLAYER_ADDED, playerId: event.playerId };
+ };
+*/
+
+// flow example
+/*
+on: {
+  PLAYER_JOINED: {
+    actions: emit(({ context, event }) => ({
+      type: "PLAYER_ADDED",
+      playerId: event.playerId,
+    })),
+  },
+},
+
+where "event" passed to the emitter is "PLAYER_JOINED"
+and event emitted to the "outside world" is "PLAYER_ADDED"
+*/
