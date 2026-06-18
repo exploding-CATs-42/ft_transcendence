2a1ee7bbe4c59ca6e4b2944cbad6f8a940d4671f
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-18 21:52:31 +0200

    feat(frontend): implement friend removal functionality in FriendsTab and FriendListItem components

--- frontend/src/web/pages/ProfilePage/components/FriendsTab/FriendsTab.tsx
+++ frontend/src/web/pages/ProfilePage/components/FriendsTab/FriendsTab.tsx
@@ -1,4 +1,10 @@
-import { Button, List, SearchInput } from "components";
+import { useModal } from "hooks";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+import api from "api";
+import { getErrorMessage } from "utils";
+import { Button, ConfirmPopup, List, SearchInput } from "components";
 
 import type { FriendItem } from "../../types";
 import { FriendListItem } from "../../components";
@@ -9,6 +15,9 @@ interface Props {
 }
 
 const FriendsTab = ({ friends }: Props) => {
+  const [isOpenModal, toggleModal] = useModal();
+  const [selectedFriend, setSelectedFriend] = useState<FriendItem | null>(null);
+
   const sortedFriends = [...friends].sort((a, b) => {
     const statusOrder = {
       ACCEPTED: 0,
@@ -22,12 +31,36 @@ const FriendsTab = ({ friends }: Props) => {
     return a.user.username.localeCompare(b.user.username);
   });
 
+  const handleRemoveClick = (friend: FriendItem) => {
+    setSelectedFriend(friend);
+    toggleModal();
+  };
+
+  const handleDeleteFriendship = async (selectedFriend: FriendItem) => {
+    const friendId = selectedFriend.user.id;
+
+    try {
+      await api.friends.deleteFriendship({ userId: friendId });
+      toggleModal();
+
+      toast.success("Success");
+    } catch (error) {
+      const errmsg = getErrorMessage(error);
+      toast.error(errmsg);
+    }
+  };
+
   return (
     <>
       <List
         items={sortedFriends}
         getKey={(friend) => friend.user.id}
-        renderItem={(friend) => <FriendListItem friend={friend} />}
+        renderItem={(friend) => (
+          <FriendListItem
+            friend={friend}
+            handleRemoveClick={handleRemoveClick}
+          />
+        )}
         className={s.list}
         empty="No friends yet"
       />
@@ -35,6 +68,17 @@ const FriendsTab = ({ friends }: Props) => {
         <SearchInput />
         <Button className={s.button}>Add</Button>
       </div>
+
+      <ConfirmPopup
+        toggleModal={toggleModal}
+        isOpenModal={isOpenModal}
+        msg="Are you sure you want to remove your friend?"
+        onConfirm={() => {
+          if (selectedFriend) {
+            handleDeleteFriendship(selectedFriend);
+          }
+        }}
+      />
     </>
   );
 };
