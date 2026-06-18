b56f0905aed18572b6da54895b98a6a2273f35d6
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-18 22:37:45 +0200

    feat(frontend): add types for FriendshipRequestAction and UpdateFriendshipBody to barrel file

--- frontend/src/web/@types/api/index.ts
+++ frontend/src/web/@types/api/index.ts
@@ -1,2 +1,4 @@
 export type { UserCredentials } from "api/auth/auth";
 export type { BadRequestErrorResponse } from "./BadRequestErrorResponse";
+export type { FriendshipRequestAction } from "./friends/friends";
+export type { UpdateFriendshipBody } from "./friends/friends";
