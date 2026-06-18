1e6e11f41a61eefdd8e6b6fcdf4a17b3c4368e2c
Andrii Kovtunets <akovtune@student.codam.nl>
2026-06-18 17:48:33 +0200

    feat(backend): broadcast dealt hands to individual players

--- backend/src/sockets/broadcasters/gameBroadcaster.ts
+++ backend/src/sockets/broadcasters/gameBroadcaster.ts
@@ -1,11 +1,14 @@
 // Project level
 import {
   CountdownStartedPayload,
+  ServerPrivateEvents,
   ServerPublicEvents,
 } from "@exploding-cats/contracts";
 import { Game } from "data/types";
 import { GameOutEvents } from "game";
 import { io } from "../../app";
+import { socketsMap } from "sockets/socketsMap";
+import { Hand } from "game/eventPayloads";
 
 export function attachGameBroadcaster(game: Game) {
   const { instance: broadcaster, id: gameId } = game;
@@ -22,6 +25,14 @@ export function attachGameBroadcaster(game: Game) {
 
   broadcaster.on(GameOutEvents.COUNTDOWN_CANCELED, () => {
     io.to(gameId).emit(ServerPublicEvents.COUNTDOWN_CANCELED);
+  });
+
+  broadcaster.on(GameOutEvents.CARDS_DEALT, (event) => {
+    const hands: Hand[] = event.payload;
+    hands.forEach((hand) => {
+      const socket = socketsMap.get(hand.playerId)!;
+      socket.emit(ServerPrivateEvents.YOUR_HAND, hand);
+    });
   });
 }
 
