ae509b618c5c3053d3af47e8ff64497fcbe87527
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-19 12:56:33 +0200

    refactor(frontend): simplify exports from friends module in api index

--- frontend/src/web/@types/api/index.ts
+++ frontend/src/web/@types/api/index.ts
@@ -1,4 +1,3 @@
 export type { UserCredentials } from "api/auth/auth";
 export type { BadRequestErrorResponse } from "./BadRequestErrorResponse";
-export type { FriendshipRequestAction } from "./friends/friends";
-export type { UpdateFriendshipBody } from "./friends/friends";
+export * from "./friends/friends";
