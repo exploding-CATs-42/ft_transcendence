f45f833742b4a724ec98b9de9984f26e200d6757
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-19 19:58:35 +0200

    refactor(frontend): rename Hand entity into GraphicHand

--- frontend/src/game/entities/index.ts
+++ frontend/src/game/entities/index.ts
@@ -1,6 +1,6 @@
 export type { Player } from "./Player";
 export { GraphicPlayer } from "./GraphicPlayer";
 export { OpponentHand } from "./OpponentHand";
-export { Hand } from "./Hand";
+export { GraphicHand } from "./Hand";
 export { PlayerSeat } from "./PlayerSeat";
 export { Button } from "./Button";
