22f28ee604592f4e0fd6060cbc58ae4b0255ef49
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-20 14:42:52 +0200

    refactor(frontend): move friend actions to FriendControl component

--- frontend/src/web/pages/ProfilePage/components/FriendControl/FriendControl.tsx
+++ frontend/src/web/pages/ProfilePage/components/FriendControl/FriendControl.tsx
@@ -1,5 +1,55 @@
-const FriendControl = () => {
-  return <></>;
+//Project level
+import type { UserId } from "@exploding-cats/contracts";
+import { Button, Icon } from "components";
+//Local level
+import type { FriendItem } from "pages/ProfilePage/types";
+import s from "./FriendControl.module.css";
+
+interface Props {
+  friend: FriendItem;
+  friendActions: {
+    remove: (friendId: UserId) => void;
+    accept: (friendId: UserId) => Promise<void>;
+    reject: (friendId: UserId) => Promise<void>;
+  };
+}
+
+const FriendControl = ({ friend, friendActions }: Props) => {
+  return (
+    <>
+      <div className={s.rightContent}>
+        {friend.status === "ACCEPTED" ? (
+          <Button
+            className={s.closeButton}
+            onClick={() => {
+              friendActions?.remove(friend.user.id);
+            }}
+          >
+            <Icon name="cross" stroke="black" width={20} height={20} />
+          </Button>
+        ) : (
+          <>
+            <Button
+              className={s.button}
+              onClick={() => {
+                friendActions?.accept(friend.user.id);
+              }}
+            >
+              Accept
+            </Button>
+            <Button
+              className={s.button}
+              onClick={() => {
+                friendActions?.reject(friend.user.id);
+              }}
+            >
+              Ignore
+            </Button>
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default FriendControl;
