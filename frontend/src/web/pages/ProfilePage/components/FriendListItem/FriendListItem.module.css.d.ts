93bcd181177ec184bf48d96a4300f7fe47d07dbc
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-20 14:44:46 +0200

    refactor(frontend): remove unused styles and components from FriendListItem

--- frontend/src/web/pages/ProfilePage/components/FriendListItem/FriendListItem.module.css.d.ts
+++ frontend/src/web/pages/ProfilePage/components/FriendListItem/FriendListItem.module.css.d.ts
@@ -1,11 +1,8 @@
 declare const styles: {
-  readonly "button": string;
-  readonly "closeButton": string;
   readonly "container": string;
   readonly "infoContainer": string;
   readonly "item": string;
   readonly "leftContent": string;
-  readonly "rightContent": string;
   readonly "status": string;
   readonly "username": string;
 };
