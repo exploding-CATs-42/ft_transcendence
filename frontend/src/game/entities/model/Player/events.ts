e4b4b2faafad506937594a09b769c45507a765f8
Andrii Kovtunets <akovtune@student.codam.nl>
2026-06-25 14:09:42 +0200

    feat(frontend): add CARD_DRAWN and CARD_TAKEN out events
    These events will be ommitted by the machine if the respective
    "in" events were successfull

--- frontend/src/game/entities/model/Player/events.ts
+++ frontend/src/game/entities/model/Player/events.ts
@@ -11,3 +11,16 @@ export type PlayerEvents = (typeof PlayerEvents)[keyof typeof PlayerEvents];
 export type PlayerEvent =
   | { type: typeof PlayerEvents.TAKE_CARD; card: Card }
   | { type: typeof PlayerEvents.DRAW_CARD; card: Card };
+
+// Events emitted FROM the machine
+export const PlayerOutEvents = {
+  CARD_DRAWN: "CARD_DRAWN",
+  CARD_TAKEN: "CARD_TAKEN",
+} as const;
+
+export type PlayerOutEvents =
+  (typeof PlayerOutEvents)[keyof typeof PlayerOutEvents];
+
+export type PlayerOutEvent =
+  | { type: typeof PlayerOutEvents.CARD_DRAWN; card: Card }
+  | { type: typeof PlayerOutEvents.CARD_TAKEN; card: Card };
