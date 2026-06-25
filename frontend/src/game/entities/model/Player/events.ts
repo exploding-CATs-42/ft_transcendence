2b2060123558988765e78250fe546f75be9324b7
Andrii Kovtunets <akovtune@student.codam.nl>
2026-06-25 17:25:00 +0200

    feat(frontend): add "in-lobby" state events

--- frontend/src/game/entities/model/Player/events.ts
+++ frontend/src/game/entities/model/Player/events.ts
@@ -2,6 +2,9 @@ import type { Card } from "@exploding-cats/game-core";
 
 // Events sent TO the machine
 export const PlayerEvents = {
+  CONFIRM_READINESS: "CONFIRM_READINESS",
+  CANCEL_READINESS: "CANCEL_READINESS",
+  GAME_STARTED: "GAME_STARTED",
   DRAW_CARD: "DRAW_CARD",
   TAKE_CARD: "TAKE_CARD",
 } as const;
@@ -9,11 +12,16 @@ export const PlayerEvents = {
 export type PlayerEvents = (typeof PlayerEvents)[keyof typeof PlayerEvents];
 
 export type PlayerEvent =
+  | { type: typeof PlayerEvents.CONFIRM_READINESS; playerId: string }
+  | { type: typeof PlayerEvents.CANCEL_READINESS; playerId: string }
+  | { type: typeof PlayerEvents.GAME_STARTED }
   | { type: typeof PlayerEvents.TAKE_CARD; card: Card }
   | { type: typeof PlayerEvents.DRAW_CARD; card: Card };
 
 // Events emitted FROM the machine
 export const PlayerOutEvents = {
+  READINESS_CONFIRMED: "READINESS_CONFIRMED",
+  READINESS_CANCELED: "READINESS_CANCELED",
   CARD_DRAWN: "CARD_DRAWN",
   CARD_TAKEN: "CARD_TAKEN",
 } as const;
@@ -22,5 +30,7 @@ export type PlayerOutEvents =
   (typeof PlayerOutEvents)[keyof typeof PlayerOutEvents];
 
 export type PlayerOutEvent =
+  | { type: typeof PlayerOutEvents.READINESS_CONFIRMED; playerId: string }
+  | { type: typeof PlayerOutEvents.READINESS_CANCELED; playerId: string }
   | { type: typeof PlayerOutEvents.CARD_DRAWN; card: Card }
   | { type: typeof PlayerOutEvents.CARD_TAKEN; card: Card };
