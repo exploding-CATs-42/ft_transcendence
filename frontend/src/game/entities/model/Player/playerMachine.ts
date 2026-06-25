85c0b94eed9b55fb40995956b1b91f48921230e4
Andrii Kovtunets <akovtune@student.codam.nl>
2026-06-25 17:26:10 +0200

    feat(frontend): implement "in-lobby" state transitions flow

--- frontend/src/game/entities/model/Player/playerMachine.ts
+++ frontend/src/game/entities/model/Player/playerMachine.ts
@@ -6,6 +6,8 @@ import { PlayerActions } from "./actions";
 import { PlayerGuards } from "./guards";
 import { machineId } from "./constants";
 import { PlayerStates } from "./states";
+import { PlayerTargets } from "./targets";
+import { PlayerEvents } from "./events";
 
 export const playerMachine = setup({
   types: {
@@ -25,7 +27,28 @@ export const playerMachine = setup({
   context: ({ input }) => input,
   initial: PlayerStates.IN_LOBBY,
   states: {
-    [PlayerStates.IN_LOBBY]: {},
+    [PlayerStates.IN_LOBBY]: {
+      initial: PlayerStates.NOT_READY,
+      states: {
+        [PlayerStates.NOT_READY]: {
+          on: {
+            [PlayerEvents.CONFIRM_READINESS]: {
+              target: PlayerTargets.READY,
+            },
+          },
+        },
+        [PlayerStates.READY]: {
+          on: {
+            [PlayerEvents.CANCEL_READINESS]: {
+              target: PlayerTargets.NOT_READY,
+            },
+            [PlayerEvents.GAME_STARTED]: {
+              target: PlayerTargets.IN_GAME,
+            },
+          },
+        },
+      },
+    },
     [PlayerStates.IN_GAME]: {},
     [PlayerStates.AFTER_GAME]: { type: "final" },
   },
