8ceea5bff19b07d2d21aad6c5bc8e6473627b7fe
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-06-26 16:44:40 +0200

    feat(frontend): add PlayerInstance type

--- frontend/src/game/entities/model/Player/factory.ts
+++ frontend/src/game/entities/model/Player/factory.ts
@@ -1,11 +1,13 @@
 // Libraries
-import { createActor } from "xstate";
+import { Actor, createActor } from "xstate";
 // Local level
 import { playerMachine } from "./playerMachine";
 import type { PlayerContext } from "./context";
 import { getStrategy } from "./strategies";
 
-export function createPlayerMachine(input: PlayerContext) {
+export type PlayerInstance = Actor<typeof playerMachine>;
+
+export function createPlayerMachine(input: PlayerContext): PlayerInstance {
   const strategy = getStrategy(input);
 
   const machine = playerMachine.provide({
