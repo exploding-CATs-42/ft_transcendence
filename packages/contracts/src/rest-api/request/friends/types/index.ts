7fa24c23658147adf7b2b1aca79424bd6a8b4b77
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-26 16:10:55 +0200

    refactor(frontend): move types and schemas into separate directories

---
+++ packages/contracts/src/rest-api/request/friends/types/index.ts
@@ -0,0 +1,5 @@
+export * from "./FriendshipView";
+export * from "./FriendshipDirection";
+export * from "./FriendshipRequestAction";
+export * from "./FriendshipStatus";
+export * from "./FriendItem";
