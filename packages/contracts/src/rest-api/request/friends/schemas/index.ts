f9712d34474a9a57c65f4e8a5ddf9b680066be7b
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-27 12:44:52 +0200

    refactor: move friends types to shared directory

--- packages/contracts/src/rest-api/request/friends/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./createFriendRequestSchema";
-export * from "./deleteFriendshipSchema";
-export * from "./updateFriendshipSchema";
-export * from "./listFriendsSchema";
-export * from "./userIdParamsSchema";
-export * from "./userIdBodySchema";
