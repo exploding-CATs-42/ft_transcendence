6a5e6e15533f7e52acaaf66f82fc4ffcbd09d584
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-27 12:44:52 +0200

    refactor: move friends types to shared directory

--- packages/contracts/src/shared/index.ts
+++ packages/contracts/src/shared/index.ts
@@ -1,2 +1,9 @@
 export type { UserId, User, PublicUser } from "./users";
 export type { AccessToken } from "./tokens";
+export type { FriendItem } from "./friends";
+export {
+  FriendshipDirection,
+  FriendshipStatus,
+  FriendshipView,
+  FriendshipRequestAction,
+} from "./friends";
