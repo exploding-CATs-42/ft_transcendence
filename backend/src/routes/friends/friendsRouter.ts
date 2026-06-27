2045980eec4c0a8b87a88e97d71d2b3d7c9cdaa5
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-27 09:49:05 +0200

    docs(backend): add explanation about authentication to friendsRouter

--- backend/src/routes/friends/friendsRouter.ts
+++ backend/src/routes/friends/friendsRouter.ts
@@ -9,6 +9,21 @@ import { createAuthenticatedRouter, errorHandler } from "utils";
 
 export const friendsRouter = createAuthenticatedRouter();
 
+/**
+ * ⚠️ All routes defined in this router require authentication.
+ *
+ * If a request reaches this router, it means:
+ * - The user has already been authenticated
+ * - Unauthorized requests are rejected before reaching this layer
+ *
+ * Authentication is handled by `router.use(authMiddleware)` call
+ * inside `createAuthenticatedRouter()`.
+ * And in case of successful authentication
+ * the request is being transformed from Request type to AuthenticatedRequest
+ *
+ * For more info see authenticatedRouter.ts
+ */
+
 friendsRouter.get("/", errorHandler(listFriendsController));
 friendsRouter.post("/", errorHandler(createFriendRequestController));
 friendsRouter.patch("/:userId", errorHandler(updateFriendshipController));
