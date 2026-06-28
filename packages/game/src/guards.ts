0a934b4702986aee33f98364d317e42c885a5c36
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-28 13:12:07 +0200

    feat(backend): add has enough cards guard

--- packages/game/src/guards.ts
+++ packages/game/src/guards.ts
@@ -4,6 +4,7 @@ import { MIN_PLAYERS } from "./constants";
 
 export const GameGuards = {
   HAS_ENOUGH_PLAYERS: "hasEnoughPlayers",
+  HAS_ENOUGH_CARDS: "hasEnoughCards",
 } as const;
 
 export interface GameGuardArgs {
@@ -15,4 +16,8 @@ export const hasEnoughPlayers = ({ context }: GameGuardArgs) => {
     context.players.length >= MIN_PLAYERS &&
     context.players.every((p) => p.isConfirmed)
   );
+};
+
+export const hasEnoughCards = ({ context }: GameGuardArgs) => {
+  return context.deck.length >= 1;
 };
