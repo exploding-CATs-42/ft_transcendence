f4deb2d26d217c3551fd21f4a631b08e636eea07
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-06-28 15:58:49 +0200

    feat(backend): add server and client socket events

--- packages/contracts/src/sockets/events.ts
+++ packages/contracts/src/sockets/events.ts
@@ -3,6 +3,7 @@ export const ClientEvents = {
   LEAVE_GAME: "leave-game",
   CONFIRM_START: "confirm-start",
   CANCEL_START: "cancel-start",
+  DRAW_CARD: "draw-card",
 } as const;
 
 export const ServerPublicEvents = {
@@ -12,13 +13,13 @@ export const ServerPublicEvents = {
   PLAYER_CANCELED: "player-canceled",
   COUNTDOWN_STARTED: "countdown-started",
   COUNTDOWN_CANCELED: "countdown-canceled",
+  CARD_DRAWN: "card-drawn",
   GAME_STARTED: "game-started",
   TURN_CHANGED: "TURN_CHANGED",
   CARD_PLAYED: "CARD_PLAYED",
   COMBO_PLAYED: "COMBO_PLAYED",
   NOPE_PLAYED: "NOPE_PLAYED",
   NOPE_WINDOW_RESOLVED: "NOPE_WINDOW_RESOLVED",
-  CARD_DRAWN: "CARD_DRAWN",
   EXPLODING_KITTEN_DRAWN: "EXPLODING_KITTEN_DRAWN",
   PLAYER_DEFUSED: "PLAYER_DEFUSED",
   PLAYER_ELIMINATED: "PLAYER_ELIMINATED",
@@ -33,7 +34,7 @@ export const ServerPrivateEvents = {
   LEFT_GAME: "left-game",
   WAITING_STATE: "waiting-state",
   YOUR_HAND: "YOUR_HAND",
-  CARD_RECEIVED: "CARD_RECEIVED",
+  CARD_RECEIVED: "card-received",
   CARD_REMOVED: "CARD_REMOVED",
   SEE_THE_FUTURE_PEEK: "SEE_THE_FUTURE_PEEK",
   DEFUSE_PROMPT: "DEFUSE_PROMPT",
@@ -46,4 +47,5 @@ export const ServerErrorEvents = {
   LEAVE_GAME_ERROR: "leave-game-error",
   CONFIRM_START_ERROR: "confirm-start-error",
   CANCEL_START_ERROR: "cancel-start-error",
+  DRAW_CARD_ERROR: "draw-card-error",
 } as const;
