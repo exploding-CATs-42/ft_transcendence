3f1ef6b3b01df6c1563de63596a373517131df07
Andrii Kovtunets <akovtune@student.codam.nl>
2026-06-29 19:26:36 +0200

    chore(frontend): add mock cards to the GameRoom scene

--- frontend/src/game/scenes/GameRoom.ts
+++ frontend/src/game/scenes/GameRoom.ts
@@ -34,7 +34,7 @@ import {
   type GameRoomHandlers,
   type GameStartedPayload,
 } from "../sockets";
-import { fakePlayers } from "./mockData";
+import { fakeCards, fakePlayers } from "./mockData";
 
 // Opponents
 const NAME_LABEL_CONFIG: LabelConfig = {
@@ -80,7 +80,7 @@ export class GameRoom extends Scene implements GameRoomHandlers {
   #detachSockets: CleanupFunction;
   #tempDataStorage: GameStartedPayload = {
     players: fakePlayers,
-    hand: { cards: [] },
+    hand: { cards: fakeCards },
   };
 
   constructor() {
