42dffc0a38ee3bc233abcfcf7cfd3a0fed790159
Andrii Kovtunets <akovtune@student.codam.nl>
2026-06-29 19:25:52 +0200

    chore(frontend): extract mock players data into a separate file, that has to be removed later

---
+++ frontend/src/game/scenes/mockData.ts
@@ -0,0 +1,7 @@
+export const fakePlayers = [
+  { id: "1", avatarUrl: null, isAlive: true, name: "me" },
+  { id: "2", avatarUrl: null, isAlive: true, name: "player1" },
+  { id: "3", avatarUrl: null, isAlive: true, name: "player2" },
+  { id: "4", avatarUrl: null, isAlive: true, name: "player3" },
+  { id: "5", avatarUrl: null, isAlive: true, name: "player4" },
+];
