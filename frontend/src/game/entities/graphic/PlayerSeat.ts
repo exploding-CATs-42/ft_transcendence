817e6d2d9acfdea5ded3dbc37d63d6866cd23d03
Andrii Kovtunets <akovtune@student.codam.nl>
2026-07-01 15:56:45 +0200

    feat(frontend): initialize target icon during PlayerSeat construction

--- frontend/src/game/entities/graphic/PlayerSeat.ts
+++ frontend/src/game/entities/graphic/PlayerSeat.ts
@@ -1,17 +1,23 @@
 import type { Point } from "game/@types";
+import { Textures } from "game/constants";
 import type { GraphicPlayer, OpponentHand } from "game/entities/graphic";
+
+const TARGET_ICON_OFFSET: Point = {
+  x: 30,
+  y: 156,
+};
 
 export class PlayerSeat {
   #container: Phaser.GameObjects.Container;
   player: GraphicPlayer | null;
   hand: OpponentHand | null;
-  targetIcon: Phaser.GameObjects.Image | null;
+  targetIcon: Phaser.GameObjects.Image;
 
   constructor(scene: Phaser.Scene, position: Point) {
     this.#container = scene.add.container(position.x, position.y);
     this.player = null;
     this.hand = null;
-    this.targetIcon = null;
+    this.targetIcon = this.createTargetIcon(scene);
   }
 
   addPlayer(player: GraphicPlayer) {
@@ -37,5 +43,14 @@ export class PlayerSeat {
 
   moveTo(position: Point) {
     this.#container.setPosition(position.x, position.y);
+  }
+
+  private createTargetIcon(scene: Phaser.Scene) {
+    const { x, y } = TARGET_ICON_OFFSET;
+    const image = scene.add.image(x, y, Textures.targetIcon);
+    image.setVisible(false);
+    this.#container.add(image);
+
+    return image;
   }
 }
