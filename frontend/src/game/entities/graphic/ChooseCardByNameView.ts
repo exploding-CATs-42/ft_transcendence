e249760d33b86b9ec12eeb3bbf2e95dc3ad6d9ed
Andrii Kovtunets <akovtune@student.codam.nl>
2026-07-02 17:04:45 +0200

    feat(frontend): add label to GraphicCardOption

--- frontend/src/game/entities/graphic/ChooseCardByNameView.ts
+++ frontend/src/game/entities/graphic/ChooseCardByNameView.ts
@@ -12,15 +12,27 @@ const ICON_OFFSET: Point = {
   y: 0,
 };
 
+const LABEL_OFFSET: Point = {
+  x: 0,
+  y: 60,
+};
+
 class GraphicCardOption extends Phaser.GameObjects.Container {
   constructor(scene: Phaser.Scene, position: Point, cardOption: CardOption) {
     super(scene);
     const icon = this.addIcon(scene, cardOption.iconTextureKey);
+    const label = this.addLabel(scene, cardOption.label);
   }
 
   private addIcon(scene: Phaser.Scene, textureKey: string) {
     const { x, y } = ICON_OFFSET;
     const icon = scene.add.image(x, y, textureKey);
     return icon;
+  }
+
+  private addLabel(scene: Phaser.Scene, text: string) {
+    const { x, y } = LABEL_OFFSET;
+    const label = scene.add.text(x, y, text).setOrigin(0, 0);
+    return label;
   }
 }
