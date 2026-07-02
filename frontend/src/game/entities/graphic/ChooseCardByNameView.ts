941b198e3d15f06003b514ec6e2c1ef5cdbb9a37
Andrii Kovtunets <akovtune@student.codam.nl>
2026-07-02 16:58:25 +0200

    feat(frontend): add GraphicCardOption class

--- frontend/src/game/entities/graphic/ChooseCardByNameView.ts
+++ frontend/src/game/entities/graphic/ChooseCardByNameView.ts
@@ -1,5 +1,13 @@
+import type { CardOption, Point } from "game/@types";
+
 export class ChooseCardByNameView extends Phaser.GameObjects.Container {
   constructor(scene: Phaser.Scene) {
+    super(scene);
+  }
+}
+
+class GraphicCardOption extends Phaser.GameObjects.Container {
+  constructor(scene: Phaser.Scene, position: Point, cardOption: CardOption) {
     super(scene);
   }
 }
