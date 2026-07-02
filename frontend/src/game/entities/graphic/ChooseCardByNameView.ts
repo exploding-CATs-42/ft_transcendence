6197c39a7ee2874fa833e5e9d64d40a6f737991c
Andrii Kovtunets <akovtune@student.codam.nl>
2026-07-02 17:05:42 +0200

    feat(frontend): set GraphicCardOption container's position

--- frontend/src/game/entities/graphic/ChooseCardByNameView.ts
+++ frontend/src/game/entities/graphic/ChooseCardByNameView.ts
@@ -23,6 +23,7 @@ class GraphicCardOption extends Phaser.GameObjects.Container {
     const icon = this.addIcon(scene, cardOption.iconTextureKey);
     const label = this.addLabel(scene, cardOption.label);
     this.add([icon, label]);
+    this.setPosition(position.x, position.y);
   }
 
   private addIcon(scene: Phaser.Scene, textureKey: string) {
