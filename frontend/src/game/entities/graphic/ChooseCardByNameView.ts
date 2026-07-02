ba3353bccb6e0c772f9322db9c80d1316f1ba4fb
Andrii Kovtunets <akovtune@student.codam.nl>
2026-07-02 17:03:20 +0200

    feat(frontend): add icon to GraphicCardOption

--- frontend/src/game/entities/graphic/ChooseCardByNameView.ts
+++ frontend/src/game/entities/graphic/ChooseCardByNameView.ts
@@ -6,8 +6,21 @@ export class ChooseCardByNameView extends Phaser.GameObjects.Container {
   }
 }
 
+// ------------------- CONFIGURATION -------------------
+const ICON_OFFSET: Point = {
+  x: 50,
+  y: 0,
+};
+
 class GraphicCardOption extends Phaser.GameObjects.Container {
   constructor(scene: Phaser.Scene, position: Point, cardOption: CardOption) {
     super(scene);
+    const icon = this.addIcon(scene, cardOption.iconTextureKey);
+  }
+
+  private addIcon(scene: Phaser.Scene, textureKey: string) {
+    const { x, y } = ICON_OFFSET;
+    const icon = scene.add.image(x, y, textureKey);
+    return icon;
   }
 }
