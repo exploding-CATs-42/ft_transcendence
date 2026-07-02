758e776f98e6d816598b363c37df25cfcae2c618
Andrii Kovtunets <akovtune@student.codam.nl>
2026-07-02 16:50:28 +0200

    feat(frontend): add CardOption type

---
+++ frontend/src/game/@types/CardOption.ts
@@ -0,0 +1,4 @@
+export interface CardOption {
+  iconTextureKey: string;
+  label: string;
+}
--- frontend/src/game/@types/index.ts
+++ frontend/src/game/@types/index.ts
@@ -4,3 +4,4 @@ export type { SpacingConfig } from "./SpacingConfig";
 export type { Size } from "./Size";
 export type { CardConfig } from "./CardConfig";
 export type { Player } from "./Player";
+export type { CardOption } from "./CardOption";
