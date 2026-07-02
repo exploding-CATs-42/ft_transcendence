9a59ae10c118b8917e2d5fcda7168f134f59b710
rmzvr <rmzvr@proton.me>
2026-07-02 12:09:14 +0200

    refactor(backend): remove unused hand payloads

--- packages/contracts/src/sockets/index.ts
+++ packages/contracts/src/sockets/index.ts
@@ -1,5 +1,4 @@
 export * from "./events";
 export * from "./views";
 export * from "./payloads";
-export type { HandPayload } from "@exploding-cats/game-core";
 export type { TurnChangedPayload } from "@exploding-cats/game-core";
