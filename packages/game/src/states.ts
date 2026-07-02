6a5de590e4b89cb24a672f5b416ffb56c5bcf40e
rmzvr <rmzvr@proton.me>
2026-07-02 11:52:24 +0200

    feat(backend): remove DEALING_CARDS state from game machine

--- packages/game/src/states.ts
+++ packages/game/src/states.ts
@@ -3,7 +3,6 @@ export const GameStates = {
   WAITING_CONFIRMING: "confirming",
   WAITING_STARTING: "starting",
   PLAYING: "playing",
-  DEALING_CARDS: "dealingCards",
   CHANGING_TURN: "changingTurn",
   WAITING_FOR_PLAYER_ACTIONS: "waitingForPlayerActions",
 } as const;
