4bc2f6acb8cdf16cc542061066111df492dc6b4f
Andrii Kovtunets <akovtune@student.codam.nl>
2026-07-02 14:27:24 +0200

    feat(frontend): add cards to SeeTheFutureView

--- frontend/src/game/entities/graphic/SeeTheFutureView.ts
+++ frontend/src/game/entities/graphic/SeeTheFutureView.ts
@@ -1,7 +1,50 @@
-import { type Card } from "@exploding-cats/game-core";
+import { type Card, CardType } from "@exploding-cats/game-core";
+import type { CardConfig, Point } from "game/@types";
+import { CARD_TYPE_TO_FRAME } from "game/constants";
+import { addCardVisual, getCardFrame } from "game/utils";
+
+// -------------- CARD CONFIGURATION --------------
+const CARD_WIDTH = 186 * 1.5;
+const CARD_HEIGHT = 260 * 1.5;
+const CARD_BORDER_RADIUS = 20;
+
+// ------------------ SPACING ------------------
+const SPACING_BETWEEN_CARDS = 400;
 
 export class SeeTheFutureView extends Phaser.GameObjects.Container {
   constructor(scene: Phaser.Scene, cards: Card[]) {
     super(scene);
+    this.addCards(scene, cards);
+  }
+
+  // ------------------ INITIALIZATION ------------------
+
+  private addCards(scene: Phaser.Scene, cards: Card[]) {
+    const pos: Point = { x: 0, y: 0 };
+    cards.forEach((card) => {
+      const cardObject = this.addCard(scene, pos, card.type);
+      this.add(cardObject);
+      pos.x += SPACING_BETWEEN_CARDS;
+    });
+  }
+
+  // ------------------ UTILS ------------------
+
+  private addCard(scene: Phaser.Scene, position: Point, type: CardType) {
+    const frameIndex = CARD_TYPE_TO_FRAME[type];
+    const frame = getCardFrame(scene, frameIndex);
+    const cardConfig = this.buildCardConfig(frame);
+    const card = addCardVisual(scene, position, cardConfig, CARD_BORDER_RADIUS);
+    return card;
+  }
+
+  private buildCardConfig(frame: Phaser.Textures.Frame): CardConfig {
+    return {
+      frame: frame,
+      size: {
+        width: CARD_WIDTH,
+        height: CARD_HEIGHT,
+      },
+    };
   }
 }
