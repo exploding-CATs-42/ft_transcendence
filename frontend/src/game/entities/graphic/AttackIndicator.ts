11ba2b6d0226ed642ba0209badca565d731f2030
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-07-04 19:31:49 +0200

    feat(frontend): add "Attack" icon to AttackIndicator

--- frontend/src/game/entities/graphic/AttackIndicator.ts
+++ frontend/src/game/entities/graphic/AttackIndicator.ts
@@ -1,7 +1,19 @@
 import type { Point } from "game/@types";
+import { Textures } from "game/constants";
 
 export class AttackIndicator extends Phaser.GameObjects.Container {
   constructor(scene: Phaser.Scene, position: Point) {
     super(scene, position.x, position.y);
+    const icon = this.addIcon(scene);
+    this.add(icon);
+  }
+
+  // --------------- Utils ---------------
+
+  private addIcon(scene: Phaser.Scene) {
+    const icon = scene.add
+      .image(0, 0, Textures.attackIcon)
+      .setDisplaySize(60, 68);
+    return icon;
   }
 }
