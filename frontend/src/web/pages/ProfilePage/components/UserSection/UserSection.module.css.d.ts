750d6a81aef8661320d4ff93f55fdb468bf03b37
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-07-04 19:23:01 +0200

    feat(frontend): introduce nameContainer for the name and copy button

--- frontend/src/web/pages/ProfilePage/components/UserSection/UserSection.module.css.d.ts
+++ frontend/src/web/pages/ProfilePage/components/UserSection/UserSection.module.css.d.ts
@@ -1,6 +1,7 @@
 declare const styles: {
   readonly "avatar": string;
-  readonly "name": string;
+  readonly "copyIcon": string;
+  readonly "nameContainer": string;
   readonly "section": string;
 };
 export = styles;
