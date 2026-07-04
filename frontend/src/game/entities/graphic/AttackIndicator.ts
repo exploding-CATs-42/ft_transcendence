1cb50790318bbe1b69d2042ee885401a0c8c9432
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-07-04 20:36:57 +0200

    feat(frontend): add white outline to the attack icon

--- frontend/src/game/entities/graphic/AttackIndicator.ts
+++ frontend/src/game/entities/graphic/AttackIndicator.ts
@@ -1,6 +1,10 @@
 import type { Point } from "game/@types";
 import { Textures } from "game/constants";
 
+const ICON_OUTLINE_COLOR = 0xffffff;
+const ICON_OUTLINE_OFFSET: Point = { x: 0, y: 3 };
+const ICON_OUTLINE_RADIUS = 32;
+
 export class AttackIndicator extends Phaser.GameObjects.Container {
   constructor(scene: Phaser.Scene, position: Point) {
     super(scene, position.x, position.y);
@@ -11,9 +15,19 @@ export class AttackIndicator extends Phaser.GameObjects.Container {
   // --------------- Utils ---------------
 
   private addIcon(scene: Phaser.Scene) {
+    const iconContainer = scene.add.container();
+
     const icon = scene.add
       .image(0, 0, Textures.attackIcon)
       .setDisplaySize(60, 68);
-    return icon;
+    // Outline
+    const { x: oX, y: oY } = ICON_OUTLINE_OFFSET;
+    const outline = scene.add.graphics();
+    outline.fillStyle(ICON_OUTLINE_COLOR, 1);
+    outline.fillCircle(oX, oY, ICON_OUTLINE_RADIUS);
+
+    iconContainer.add([outline, icon]);
+
+    return iconContainer;
   }
 }
