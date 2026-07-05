fea114f4b1204086aa8eb1e8e0b97280da319c21
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-07-05 14:18:03 +0200

    feat(frontend): add background to SkipView

--- frontend/src/game/entities/graphic/SkipView.ts
+++ frontend/src/game/entities/graphic/SkipView.ts
@@ -1,5 +1,29 @@
+const BACKGROUND_CONFIG = {
+  width: 780,
+  height: 300,
+  color: 0x2ea3dc,
+  borderRadius: 20,
+};
 export class SkipView extends Phaser.GameObjects.Container {
   constructor(scene: Phaser.Scene, _playerName: string) {
     super(scene);
+    const background = this.addBackground(scene);
+    this.add([background]);
+  }
+
+  private addBackground(scene: Phaser.Scene) {
+    const { width, height, color, borderRadius } = BACKGROUND_CONFIG;
+
+    const graphics = scene.add.graphics();
+    graphics.fillStyle(color, 1);
+    graphics.fillRoundedRect(
+      -width / 2,
+      -height / 2,
+      width,
+      height,
+      borderRadius,
+    );
+
+    return graphics;
   }
 }
