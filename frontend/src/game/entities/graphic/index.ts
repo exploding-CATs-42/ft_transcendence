abc3337c7b49ec4111208f3b65c929aaa3e093ce
lemonnn616 <riadynskyiivan@gmail.com>
2026-07-16 15:04:23 +0300

    feat: add kind combo selection

--- frontend/src/game/entities/graphic/index.ts
+++ frontend/src/game/entities/graphic/index.ts
@@ -1,6 +1,6 @@
 export { GraphicPlayer } from "./GraphicPlayer";
 export { OpponentHand } from "./OpponentHand";
-export { GraphicHand } from "./GraphicHand";
+export { GraphicHand, type KindComboSelection } from "./GraphicHand";
 export { PlayerSeat } from "./PlayerSeat";
 export { Button } from "./Button";
 export type { GraphicCard } from "./GraphicCard";
