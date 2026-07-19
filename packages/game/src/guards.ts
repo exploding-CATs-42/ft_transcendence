f0e8b4cb7bd57b5f73ba3632549f3c27ed76d437
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-07-19 16:41:14 +0200

    feat(backend): add isCardType guard to validate card type in PLAY_CARD event

--- packages/game/src/guards.ts
+++ packages/game/src/guards.ts
@@ -1,15 +1,19 @@
 // Local level
 import type { GameContext } from "./gameMachine";
 import { MIN_PLAYERS } from "./constants";
+import { GameEvent, GameEvents } from "./events";
+import { CardType } from "./types";
 
 export const GameGuards = {
   HAS_ENOUGH_PLAYERS: "hasEnoughPlayers",
   HAS_ENOUGH_CARDS: "hasEnoughCards",
+  IS_CARD_TYPE: "isCardType",
   IS_ENOUGH_CARDS_IN_DECK: "isEnoughCardsInDeck",
 } as const;
 
 export interface GameGuardArgs {
   context: GameContext;
+  event: GameEvent;
 }
 
 export const hasEnoughPlayers = ({ context }: GameGuardArgs) => {
@@ -29,4 +33,12 @@ export const hasEnoughCards = ({ context }: GameGuardArgs): boolean => {
   );
 
   return (player?.hand.length ?? 0) > 0;
+};
+
+export const isCardType = (
+  { event }: GameGuardArgs,
+  params: { cardType: CardType },
+) => {
+  if (event.type !== GameEvents.PLAY_CARD) return false;
+  return event.card.type === params.cardType;
 };
