79f9024c6814f3c15fffc59eece06833b79b0221
Andrii Kovtunets <andrejkovtunec@gmail.com>
2026-07-26 19:00:06 +0200

    feat(contracts): add PLAYER_SELECTED server event

--- packages/contracts/src/sockets/events.ts
+++ packages/contracts/src/sockets/events.ts
@@ -38,6 +38,7 @@ export const ServerPublicEvents = {
   DECK_SHUFFLED: "DECK_SHUFFLED",
   TURN_SKIPPED: "TURN_SKIPPED",
   GAME_OVER: "GAME_OVER",
+  PLAYER_SELECTED: "PLAYER_SELECTED",
 } as const;
 
 export const ServerPrivateEvents = {
