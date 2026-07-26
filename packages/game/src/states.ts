654250e5ceadd14da175a2ee9f3e10e20a1a360c
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-07-26 18:53:36 +0200

    feat(backend): add game states for exploding kitten mechanics

--- packages/game/src/states.ts
+++ packages/game/src/states.ts
@@ -5,4 +5,9 @@ export const GameStates = {
   PLAYING: "playing",
   CHANGING_TURN: "changingTurn",
   WAITING_FOR_PLAYER_ACTIONS: "waitingForPlayerActions",
+  CHECKING_DRAWN_CARD: "checkingDrawnCard",
+  EXPLODING_KITTEN_DRAWN: "explodingKittenDrawn",
+  WAITING_FOR_DEFUSE_CARD: "waitingForDefuseCard",
+  WAITING_FOR_KITTEN_INSERTION: "waitingForKittenInsertion",
+  EXPLODING_PLAYER: "explodingPlayer",
 } as const;
