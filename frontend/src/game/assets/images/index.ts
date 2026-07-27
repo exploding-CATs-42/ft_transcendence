6713a5307b85b0c72cbaf2cd816ff6acbd80dc02
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-07-27 15:12:30 +0200

    feat(frontend): add fire sprite sheet asset for game visuals

--- frontend/src/game/assets/images/index.ts
+++ frontend/src/game/assets/images/index.ts
@@ -11,3 +11,4 @@ export { default as shuffleSpritesheet } from "./cloud.png";
 export { default as dead } from "./dead.png";
 export { default as nopeSpritesheet } from "./nope.png";
 export { default as boomExplosionSpritesheet } from "./boom-explosion.png";
+export { default as fireSpriteSheet } from "./fire.png";
