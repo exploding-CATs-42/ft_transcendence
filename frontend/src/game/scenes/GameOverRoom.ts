16af899637501f2027c40106b7c1ac1049d45f69
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-07-28 17:02:30 +0200

    feat(frontend): implement leave game button in GameOverRoom

--- frontend/src/game/scenes/GameOverRoom.ts
+++ frontend/src/game/scenes/GameOverRoom.ts
@@ -1,11 +1,43 @@
 // Libraries
 import { Scene } from "phaser";
 // Local level
-import { type GameOverRoomHandlers } from "game/sockets";
-import { Scenes } from "game/constants";
+import { leaveFinishedGame, type GameOverRoomHandlers } from "game/sockets";
+import { Scenes, SCREEN_WIDTH } from "game/constants";
+import { Button } from "game/entities";
+import type { Point, Size } from "game/@types";
+
+const LEAVE_BUTTON_SIZE: Size = {
+  width: 260,
+  height: 72,
+};
+
+const LEAVE_BUTTON_POSITION: Point = {
+  x: SCREEN_WIDTH - LEAVE_BUTTON_SIZE.width - 24,
+  y: 24,
+};
 
 export class GameOverRoom extends Scene implements GameOverRoomHandlers {
   constructor() {
     super(Scenes.GameOverRoom);
   }
+
+  create() {
+    this.addLeaveGameButton();
+  }
+
+  private addLeaveGameButton() {
+    const button = new Button(
+      this,
+      LEAVE_BUTTON_POSITION,
+      LEAVE_BUTTON_SIZE,
+      "Leave game",
+      this.leaveGame,
+    );
+
+    button.setBackgroundColor(0xc73535);
+  }
+
+  private leaveGame = () => {
+    leaveFinishedGame();
+  };
 }
