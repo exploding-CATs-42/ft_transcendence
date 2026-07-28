455c173419707c493f712252f0d7c5149f1dfb0d
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-07-28 17:04:09 +0200

    feat(frontend): add explosion background to GameOverRoom

--- frontend/src/game/scenes/GameOverRoom.ts
+++ frontend/src/game/scenes/GameOverRoom.ts
@@ -2,10 +2,13 @@
 import { Scene } from "phaser";
 // Local level
 import { leaveFinishedGame, type GameOverRoomHandlers } from "game/sockets";
-import { Scenes, SCREEN_WIDTH } from "game/constants";
+import { Scenes, SCREEN_HEIGHT, SCREEN_WIDTH } from "game/constants";
 import { Button } from "game/entities";
 import type { Point, Size } from "game/@types";
 import { addFullscreenToggle } from "game/utils";
+import { GameOverAnimation } from "game/animations";
+
+const CENTER_X = SCREEN_WIDTH / 2;
 
 const LEAVE_BUTTON_SIZE: Size = {
   width: 260,
@@ -23,6 +26,8 @@ export class GameOverRoom extends Scene implements GameOverRoomHandlers {
   }
 
   create() {
+    this.addExplosionBackground();
+
     addFullscreenToggle(this);
     this.addLeaveGameButton();
   }
@@ -37,6 +42,16 @@ export class GameOverRoom extends Scene implements GameOverRoomHandlers {
     );
 
     button.setBackgroundColor(0xc73535);
+  }
+
+  private addExplosionBackground() {
+    const explosion = new GameOverAnimation(
+      this,
+      { x: CENTER_X, y: SCREEN_HEIGHT / 2 },
+      { width: SCREEN_WIDTH, height: SCREEN_HEIGHT },
+    );
+
+    explosion.playAnimation();
   }
 
   private leaveGame = () => {
