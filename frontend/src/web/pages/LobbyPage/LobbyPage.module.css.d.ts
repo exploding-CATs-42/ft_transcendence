59d75779e2c80b0c2a4801dccbec232d49efd3a4
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-07-30 19:07:09 +0200

    feat(frontend): update styles is d.ts file

--- frontend/src/web/pages/LobbyPage/LobbyPage.module.css.d.ts
+++ frontend/src/web/pages/LobbyPage/LobbyPage.module.css.d.ts
@@ -2,10 +2,13 @@ declare const styles: {
   readonly "button": string;
   readonly "buttons": string;
   readonly "color": string;
+  readonly "currentGameLabel": string;
+  readonly "existingGame": string;
+  readonly "gameButton": string;
   readonly "list": string;
   readonly "listSection": string;
-  readonly "gameButton": string;
   readonly "pageContainer": string;
+  readonly "separator": string;
 };
 export = styles;
 
