0b2b051ede246a3d083f70cf813366860ada96f2
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-07-30 19:37:04 +0200

    feat(backend): add PlayerStatus field to PlayerView interface

--- packages/contracts/src/sockets/views.ts
+++ packages/contracts/src/sockets/views.ts
@@ -1,8 +1,11 @@
+import { PlayerStatus } from "@exploding-cats/game-core";
+
 export interface PlayerView {
   id: string;
   name: string;
   avatarUrl: string | null;
   isConnected: boolean;
+  status: PlayerStatus;
 }
 
 export interface GamePlayerView extends PlayerView {
