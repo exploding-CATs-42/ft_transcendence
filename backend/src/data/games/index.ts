4c8f265ce83c526fe2f2136a2b43464f22643628
lemonnn616 <riadynskyiivan@gmail.com>
2026-07-31 20:01:43 +0300

    feat(backend): add game history persistence

--- backend/src/data/games/index.ts
+++ backend/src/data/games/index.ts
@@ -1,4 +1,5 @@
 export * from "./gamePersistence";
+export * from "./gameHistoryRepository";
 export { default as GameStore } from "./gameStore";
 export * from "./mappers";
 export { default as GameRepository } from "./gameRepository";
