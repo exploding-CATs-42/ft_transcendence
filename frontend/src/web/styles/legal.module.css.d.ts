201b6969bff06c6b027ac0f8708fb39a2dc292ab
rmzvr <rmzvr@proton.me>
2026-08-04 10:39:41 +0200

    feat(frontend): add styles for legal information pages

---
+++ frontend/src/web/styles/legal.module.css.d.ts
@@ -0,0 +1,10 @@
+declare const styles: {
+  readonly "list": string;
+  readonly "pageContainer": string;
+  readonly "pageHeader": string;
+  readonly "section": string;
+  readonly "sectionHeader": string;
+  readonly "text": string;
+  readonly "updated": string;
+};
+export = styles;
