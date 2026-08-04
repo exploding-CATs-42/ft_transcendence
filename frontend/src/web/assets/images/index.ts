31c24414b6ecf22690afc0881419c9b97d49c4a6
rmzvr <rmzvr@proton.me>
2026-08-04 11:55:08 +0200

    feat(frontend): add crown image asset for game winner

--- frontend/src/web/assets/images/index.ts
+++ frontend/src/web/assets/images/index.ts
@@ -1,5 +1,6 @@
 export { default as CatomicBomb } from "./catomic-bomb/catomic-bomb.png";
 export { default as FireCat } from "./fire-cat/fire-cat.png";
 export { default as Avatar } from "./avatar/avatar-193w.png";
+export { default as Crown } from "./crown.png";
 export { default as Loader } from "./loader/loader.gif";
 export { default as Spinner } from "./spinner/spinner.gif";
