21ac35c196869212ebf7f36e3622cf239020f67f
rmzvr <rmzvr@proton.me>
2026-08-04 11:55:16 +0200

    feat(frontend): add crown display for game winner in GameListItem

--- frontend/src/web/components/GameListItem/GameListItem.module.css.d.ts
+++ frontend/src/web/components/GameListItem/GameListItem.module.css.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "container": string;
+  readonly "crown": string;
   readonly "item": string;
   readonly "items": string;
   readonly "title": string;
--- frontend/src/web/components/GameListItem/GameListItem.tsx
+++ frontend/src/web/components/GameListItem/GameListItem.tsx
@@ -1,4 +1,5 @@
 // Project level
+import { Crown } from "assets";
 import { Avatar, LinkButton, ListItem } from "components";
 // Local level
 import { createGameSlots, isPlaceholderSlot } from "./slots";
@@ -19,6 +20,7 @@ const isLobbyGame = (game: GameListItemData): game is LobbyGame => {
 const GameListItem = ({ game, className }: Props) => {
   const gameId = isLobbyGame(game) ? game.id : game.gameId;
   const gameName = isLobbyGame(game) ? game.name : game.gameName;
+  const winnerId = isLobbyGame(game) ? null : game.winnerId;
   const slotCount = isLobbyGame(game)
     ? game.maxPlayers
     : GAME_HISTORY_SLOT_COUNT;
@@ -29,24 +31,30 @@ const GameListItem = ({ game, className }: Props) => {
       <div className={s.container}>
         <span className={s.title}>{gameName}</span>
         <ul className={s.items}>
-          {slots.map((slot, index) => renderSlot(slot, `${gameId}_${index}`))}
+          {slots.map((slot, index) =>
+            renderSlot(slot, `${gameId}_${index}`, winnerId),
+          )}
         </ul>
       </div>
     </ListItem>
   );
 };
 
-const renderSlot = (slot: GameSlot, key: string) => {
+const renderSlot = (slot: GameSlot, key: string, winnerId: string | null) => {
   const isPlaceholder = isPlaceholderSlot(slot);
+  const isWinner = !isPlaceholder && slot.player.id === winnerId;
 
   const item = (
     <li key={key} data-placeholder={isPlaceholder} className={s.item}>
       {!isPlaceholder && (
-        <Avatar
-          variant="game"
-          src={slot.player.avatarUrl}
-          status={slot.player.isOnline}
-        />
+        <>
+          <Avatar
+            variant="game"
+            src={slot.player.avatarUrl}
+            status={slot.player.isOnline}
+          />
+          {isWinner && <img className={s.crown} src={Crown} alt="Winner" />}
+        </>
       )}
     </li>
   );
