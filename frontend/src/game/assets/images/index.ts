4bd728148ec7eca25cb7f3e940069f27dd7462fb
akovtune <akovtune@student.codam.nl>
2026-08-04 15:19:41 +0200

    refactor(assets): move player-spotlight.png file inside assets/images folder for consistency

--- frontend/src/game/assets/images/index.ts
+++ frontend/src/game/assets/images/index.ts
@@ -15,3 +15,4 @@ export { default as fireSpriteSheet } from "./fire.webp";
 export { default as nuclearSpritesheet } from "./nuclear-loop.png";
 export { default as coolCat } from "./cool-cat.webp";
 export { default as scaleMeterBar } from "./scale_meter_bar.png";
+export { default as playerSpotlight } from "./player-spotlight.png";
