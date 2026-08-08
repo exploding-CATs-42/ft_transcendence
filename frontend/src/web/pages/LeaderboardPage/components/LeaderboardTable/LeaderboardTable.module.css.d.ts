67dde48aa31529fd99eabb5e4350ebe07cbeebfb
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-08-08 17:36:57 +0200

    feat(frontend): create LeaderboardTable component with styles

---
+++ frontend/src/web/pages/LeaderboardPage/components/LeaderboardTable/LeaderboardTable.module.css.d.ts
@@ -0,0 +1,19 @@
+declare const styles: {
+  readonly "avatar": string;
+  readonly "cell": string;
+  readonly "countHeader": string;
+  readonly "crown": string;
+  readonly "headerCell": string;
+  readonly "headerRow": string;
+  readonly "list": string;
+  readonly "listColumns": string;
+  readonly "player": string;
+  readonly "playerHeader": string;
+  readonly "rank": string;
+  readonly "row": string;
+  readonly "scrollContainer": string;
+  readonly "topRow": string;
+  readonly "username": string;
+};
+export = styles;
+
