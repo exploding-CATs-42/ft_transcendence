65be2745d0015baf6e95a70c4ae59cc5cde246be
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-08-09 10:38:53 +0200

    feat(frontend): export ProfileContext and ProfileProvider in context index

--- frontend/src/web/context/index.ts
+++ frontend/src/web/context/index.ts
@@ -2,3 +2,5 @@ export { AuthContext } from "./AuthContext";
 export { default as AuthProvider } from "./AuthProvider";
 export { SocketContext } from "./SocketContext";
 export { default as SocketProvider } from "./SocketProvider";
+export { ProfileContext } from "./ProfileContext";
+export { default as ProfileProvider } from "./ProfileProvider";
