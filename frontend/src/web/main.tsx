55976ec01fb701f1ca54f9372ca2f17d0d1c9b8b
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2026-08-09 10:42:43 +0200

    feat(frontend): add ProfileProvider to main application structure

--- frontend/src/web/main.tsx
+++ frontend/src/web/main.tsx
@@ -2,16 +2,18 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 // Project level
-import { AuthProvider, SocketProvider } from "context";
+import { AuthProvider, ProfileProvider, SocketProvider } from "context";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <SocketProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </SocketProvider>
+    <ProfileProvider>
+      <SocketProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SocketProvider>
+    </ProfileProvider>
   </AuthProvider>,
 );
